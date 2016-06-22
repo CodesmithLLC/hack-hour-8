1bcfadfab743d7553079ebf2254006dd61148d08
Colin Brownlie <Colin Brownlie>
2016-06-22 09:43:41 -0700

    fizzbuzz done

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,20 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+	//create result variable, initialized to empty array
+	var result = [];
+	//create for loop with counter starting at 0, ending at num
+	for (var i = 1; i <= num; i++){
+		//push the counter value to the array
+		if (i % 3 !== 0 && i % 5 !== 0)	result.push(i);
+		//use modulus to determine if counter is divisible by three & five, if so push 'fizzbuzz' instead of counter
+		else if (i % 3 === 0 && i % 5 === 0) result.push('fizzbuzz');
+		//use modulus to determine if counter is divisible by three, if so push 'fizz' instead of counter
+		else if (i % 3 === 0) result.push('fizz');
+		//use modulus to determine if counter is divisible by five, if so push 'buzz' instead of counter
+		else result.push('buzz');
+	}
+	return result;
 }
 
 module.exports = fizzbuzz;
