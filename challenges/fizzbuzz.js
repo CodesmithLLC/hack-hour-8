597c93c2c3a35892fdfbe33de271194acef5987d
Brandon <student@codesmiths-Mac-mini.local>
2016-06-22 09:57:45 -0700

    hack hour 1 finished

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,40 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+	// define empty array to be returned
+	var result = [];
+	// wrap function that checks divisibility of num and pushes to result array
+	function doFizzBuzz(num){
+        // check for i = 0
+        if (i === 0) result.push(0);
+		// define new empty string for every increment
+		var str1 = '';
+		// check for divisibilty by 3, if so add fizz to string
+		if (i % 3 === 0) str1 += 'fizz';
+		// check for divisibility by 5, if so add buzz to string
+		if (i % 5 === 0) str1 += 'buzz';
+		// check that number is divisible by either 3 or 5 by string length, if so push string to result array
+		if (str1.length > 0) result.push(str1);
+		// if string is empty, push the number
+		else result.push(i);
+		}
+	// if num is positive increment up
+	if (num > 0) {
+		// loop through numbers 1 to num
+		for (var i = 1; i <= num; i++) {
+			// execute fizzBuzz on each number from 1 to num
+			doFizzBuzz(i);
+	    }
+    }
+	// if num is negative increment down
+	else{
+		// loop through numbers 1 to num
+		for (var i = 1; i >= num; i--) {
+			// execute fizzBuzz on each number from 1 to num
+			doFizzBuzz(i);
+		}
+	}
+	return result;
 }
 
 module.exports = fizzbuzz;
