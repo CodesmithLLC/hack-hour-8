ccba925b6f38df2ad959ec5d69153c3a05fee5d4
Mike Puangmalai <mike@nongaap.com>
2016-06-24 09:47:32 -0700

    initial coded solution but printing wrong order

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,41 @@
 
 */
 
+/*
+Solution approach:
+Edge case: check that n is a number 1 to 100
+Variables: create a variable that holds an empty space ' ', variable that holds '*', and a variable that holds and console.logs steps
+Do a for loop that console.logs a stair case
+
+
+*/
+
 function drawStairs(n) {
+
+if(n < 1 || n > 100) {
+	return console.log('Must be a number from 1 to 100');
+}
+
+var space = ' ';
+var asterisks = '*';
+var stepArr = [];
+var step;
+
+for (var i = 0; i < n; i++) {
+	stepArr.push(asterisks);
+}
+
+step = stepArr.join().split(',').join('')
+console.log(step);
+
+for (var i = 0; i < n-1; i++) {
+	stepArr[i] = space;
+	step = stepArr.join().split(',').join('');
+	console.log(step);
+}
 
 }
 
+drawStairs(6);
 
 module.exports = drawStairs;
