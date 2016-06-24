b60841582a47648c5fa7dd576a46c0fa152634be
Shray bansal <bansalshray@gmail.com>
2016-06-24 09:20:16 -0700

    Draw-Stairs completed

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,7 +14,17 @@
 */
 
 function drawStairs(n) {
-
+	var newStr = "";
+	for(var i = 1; i <= n ; i++){
+		for(var j = 0; j < n-i ; j++){
+			newStr += " ";
+		}
+		for(var j = 0; j < i ; j++){
+			newStr += "*";
+		}
+		newStr += "\n";
+	}
+	console.log(newStr);
 }
 
 
