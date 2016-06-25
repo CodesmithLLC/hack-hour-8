4222299f58866c4fff1b00c45f41a7f92920eb7e
Zhiwen Huang <zhiwen555@gmail.com>
2016-06-25 12:42:56 -0400

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,41 @@
  */
 
 
-function modemean(array) {
+ function modemean(array) {
+ 	// find mode
+ 	// create an object for the
 
-}
+ 	var sum = 0;
+ 	var modeObj = {};
+
+ 	array.forEach(function(num) {
+ 		sum += num;
+ 		modeObj[num] = 0;
+ 	});
+
+ 	array.forEach(function(num) {
+ 		modeObj[num] += 1;
+ 	});
+
+ 	console.log(modeObj);
+ 	var mode = { num: null, count: 0 };
+ 	for (var key in modeObj) {
+ 			if (mode.num === null) {
+ 				mode.num = key;
+ 				mode.count = modeObj[key];
+ 			} else if (modeObj[key] === mode.count && Number(key) > mode.num) {
+ 				mode.num = key;
+ 				mode.count = modeObj[key];
+ 			} else if (modeObj[key] > mode.count) {
+ 				mode.num = key;
+ 				mode.count = modeObj[key];
+ 			}
+ 	}
+
+ 	console.log(mode.num);
+ 	var mean = Math.floor(sum / array.length);
+ 	console.log(mean);
+ 	return mode.num == mean ? true : false;
+ }
 
 module.exports = modemean;
