9004a5d7e9d7e7f4f3ac7990647d7080fe81f8e1
Chandra Lindy <chandra.lindy@gmail.com>
2016-06-25 09:49:01 -0700

    Hack hour 3 attempt 1

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,39 @@
 
 
 function modemean(array) {
+  let mean, sum, mode;
+  let max = 0;
+  const tally = {};
+  const podium = [];
 
+  sum = array.reduce(function(prev, curr) {
+    if (tally[curr]) tally[curr]++;
+    tally[curr] = 1;
+    return prev + curr;
+  }, 0);
+
+  mean = sum / array.length;
+  mean = Math.floor(mean);
+
+  for (let key in tally) {
+    if (tally[key] > max) {
+      max = tally[key];
+      mode = key;
+  }
+
+  for (let key in tally) {
+    if (tally[key] === max) {
+      podium.push(key);
+    }
+  }
+
+  if (podium.length > 1) {
+    mode = podium.reduce(function(prev, curr) {
+      return prev > curr ? prev : curr;
+    });
+  }
+
+  return mode === mean;
 }
 
 module.exports = modemean;
