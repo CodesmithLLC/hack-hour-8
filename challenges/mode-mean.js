295ffb9f6051c834abb8d0bdc999901178177429
Shray bansal <bansalshray@gmail.com>
2016-06-25 09:49:49 -0700

    Mode Mean Completed

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,33 @@
 
 function modemean(array) {
 
+var numObj = {};
+var modes = [];
+
+array.forEach(function(item){
+	if(numObj.hasOwnProperty(item)===false) numObj[item] =1;
+	else numObj[item] = numObj[item]+1;
+});
+
+var greatestFreq = 0;
+for(var prop in numObj){
+	if (greatestFreq < numObj[prop]) greatestFreq = numObj[prop];
+}
+
+for(var key in numObj){
+	if(numObj[key]===greatestFreq) modes.push(key);
+}
+
+var maxedMode = Math.max.apply(null, modes);
+
+var mean = array.reduce(function(accum, curr){
+	return accum+curr;
+}, 0)/array.length;
+
+var flooredMean = Math.floor(mean);
+
+if (maxedMode === flooredMean) return true;
+else return false;
 }
 
 module.exports = modemean;
