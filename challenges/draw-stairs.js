2d7f06ea3186ee68c59ca174b98c87b394134c0e
Yandri Sanchez <ysanchez@Yandris-MacBook-Pro.local>
2016-06-28 09:13:52 -0700

    draw-stairs redone

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,47 +13,49 @@
 
 */
 
-function drawStairs(n) {
-  let stairs = ""
-
-  let spacesMade = n;
-for(var i = 1; i < n; i++) {
-  stairs += makeSpace(spacesMade);
-  stairs += makeStair(i);
-
-  if(i + 1 !== n) {
-    stairs += "\n";
-  }
-
-  spacesMade--;
-}
-
-
+function drawStairs (n) {
 
   function makeStair(num) {
     let stair = "";
 
     while(num > 0) {
-      stair += "*"
-      num--
+      stair += "*";
+      num--;
     }
 
-    return stair
+    return stair;
   }
 
 
   function makeSpace(num) {
-    let spaces = ""
-    while(num > 1) {
+    let spaces = "";
+
+    while (num > 1) {
       spaces += " ";
-      num--
+      num--;
     }
 
     return spaces;
   }
 
+
+  let stairs = "";
+
+  let spacesMade = n;
+  for (let i = 1; i < n; i++) {
+    stairs += makeSpace(spacesMade);
+    stairs += makeStair(i);
+
+    if(i + 1 !== n) {
+      stairs += "\n";
+    }
+
+    spacesMade--;
+  }
+
+  stairs = stairs.replace(" ", "");
   stairs += "\n" + makeStair(n);
-  return stairs
+  return stairs;
 }
 
 
