0cb8b0c172527a369cb255a8ae92cbaf40894bb4
davidvorona <davidavorona@gmail.com>
2016-06-29 09:20:56 -0700

    finished pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,9 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
-}
+ function pow(base, power) {
+   if (power === 1) return base;
+   return base * pow(base, power - 1);
+ }
 
 module.exports = pow;
