0c285b0b7a0917be20b4597a6ba346f789f7df56
Cole Williams <cowi4030@colorado.edu>
2016-06-29 10:00:51 -0700

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,14 @@
  */
 
 function pow(base, power) {
-
+	if(power === 0) return 1
+	console.log('base', base);
+	console.log('power', power);
+ return base * pow(base, power-1)
+	
 }
+
+//base case when power<1
+pow(5,3);
 
 module.exports = pow;
