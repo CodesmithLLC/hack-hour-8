909da70c60db35aa45bdc2c5aeb5b9441492d6c0
Shray bansal <bansalshray@gmail.com>
2016-06-30 09:24:01 -0700

    Finished reverse in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -12,7 +12,13 @@
  */
 
 function reverseInPlace(array) {
+	for(var i =0; i<array.length/2; i++){
+		var temp = array[i];
+		array[i] = array[array.length -1-i];
+		array[array.length -1-i] = temp;
 
+	}
+	return array;
 }
 
 module.exports = reverseInPlace;
