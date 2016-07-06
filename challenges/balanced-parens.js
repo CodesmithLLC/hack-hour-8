51d499b1aca92e3a898abd33241ec89007ea1987
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-06 10:00:40 -0700

    first attempt

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,59 @@
  */
 
 function balancedParens(input){
+  if (input.length % 2 !== 0) return false;
+  // for (let i = 0, j = input.length - 1; i < input.length / 2; i++, j--) {
+  //   if (input.charAt(i) + input.charAt(j) !== '()') return false;
+  // }
+  // return true;
 
+  const arr = [];
+
+  if (isOpeningBracket(input.charAt(0))) arr.push(input.charAt(0));
+  else return false;
+
+  for (let i = 1, j < input.length; i++) {
+    const char = input.charAt(i);
+    if (isOpeningBracket(char)) {
+      arr.push(char);
+      break;
+    } else if (isMatched(arr[arr.length-1], char)) {
+      arr.pop();
+      break;
+    } else return false;
+  }
+
+  function isMatched(open, close) {
+    switch (open + close) {
+      case '()':
+      case '[]':
+      case '{}':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  function isOpeningBracket(char) {
+    switch (char) {
+      case '(':
+      case '[':
+      case '{':
+        return true;
+      default:
+        return false;
+    }
+  }
+  function isClosingBracket(char) {
+    switch (char) {
+      case ')':
+      case ']':
+      case '}':
+        return true;
+      default:
+        return false;
+    }
+  }
 }
 
 module.exports = balancedParens;
