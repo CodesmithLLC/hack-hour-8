f627d0c751e78577dab10e72e685a5043ab8fc2e
Zhiwen Huang <zhiwen555@gmail.com>
2016-07-06 10:02:00 -0700

    balanced pars

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,40 @@
  */
 
 function balancedParens(input){
+  let ps = 0;
+  let bs = 0;
+  let cs = 0;
+  // loop thru the array and everytime a ( is encountered put up a count
+  // if the count isn't 0 then return false;
+  const arr = input.split('');
+
+  arr.forEach(function(element) {
+    if (element == '(') {
+      ps++;
+    } else if (element == ')') {
+      ps--;
+    } else if (element == '[') {
+      bs++;
+    } else if (element == ']') {
+      bs--;
+    } else if (element == '{') {
+      cs++;
+    } else if (element == '}') {
+      cs--;
+    }
+  });
+
+  return ps === 0 && cs === 0 && bs === 0 ? true : false;
+
+  // console.log(input);
 
 }
 
-module.exports = balancedParens;
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();'))
+// console.log(balancedParens('()'))
+// console.log(balancedParens(')('))
+// console.log(balancedParens('(())'))
+// console.log(balancedParens('var hubble = function() { telescopes.awesome();'));
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }'));
+
+//module.exports = balancedParens;
