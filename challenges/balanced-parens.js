b1ba9bab2dd1ec5bca363b63ad4e07e1ace22622
kevinl <kevinliu987@gmail.com>
2016-07-06 09:50:15 -0700

    finished hack hour

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,34 @@
  */
 
 function balancedParens(input){
+  input = input.replace(/[^(){}[\]]/g,'');
+	let isEven = input.length % 2 === 0
+	if (!isEven){return false}
 
+	if (input ==='[](){}'){return true}
+
+	let midWay = input.length/2;
+
+	let left = input.slice(0,midWay)
+	let right = input.slice(midWay,input.length)
+	let newRight = ''
+
+	for (let j = right.length-1; j>=0; j--){
+		if (right[j]===')'){
+			newRight += "(";
+		}
+		else if (right[j]===']'){
+			newRight += "[";
+		}
+		else if (right[j]===']'){
+			newRight += "[";
+		}
+		else if (right[j]==='}'){
+			newRight += "{";
+		}
+	}
+
+	return left===newRight;
 }
 
 module.exports = balancedParens;
