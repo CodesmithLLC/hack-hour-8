bf854cd71c2f73f4e5303cde059da79d2eef145f
Cole Williams <cowi4030@colorado.edu>
2016-07-06 10:01:12 -0700

    balanced parents

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,38 @@
  *
  */
 
-function balancedParens(input){
+function balancedParens(input) {
+    let closedCharacters = [')', '}', ']'];
+    let pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    let openCharacters = ['(', '[', '{'];
+    let closedObj = {')': 1, '}': 1, ']': 1}
+    let empty = []; //maybe this should be a stack?
+    if (closedCharacters.indexOf(input[0]) === -1) return false
+        //if first character is closed return false
+    if(input.length === 0) return console.error('no input provided');
+        //if input doesnt exist return false;
+    if(typeof input !== 'string') return false
+        //if input isn't a string, return false
 
+        //iterate through string. if open char, push to empty. if closed, pop off last character and compare using pairs object
+    for(var i = 0; i < input.length; i++) {
+      if(openCharacters.indexOf(input[i]) !== -1) {
+        var current = input[i]
+        empty.push(current)
+      } else if (openCharacters.indexOf(current) === -1) {
+        if(pairs[empty.pop()] !== current) return false //if they aren't pairs return false
+      }
+    }
+    return true;
 }
+//start checking after ''
+//if first character after '' is a closed paren, return false
+//create and object with open characters and an object with closed characters
+//or create one object of pairs
+//create empty array or stack for storing characters as you loop through
 
 module.exports = balancedParens;
--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -7,12 +7,30 @@
  *              stringRotation("hello", "he") -> false
  */
 
-function isSubstring(s1, s2) {
-  return s1.indexOf(s2) >= 0;
-}
+// function isSubstring(s1, s2) {
+//   return s1.indexOf(s2) >= 0;
+// }
 
-function stringRotation(s1, s2) {
+// function stringRotation(s1, s2) {
 
+// }
+
+function rotationChecker(str1, str2) {
+	var str1Total = 0;
+	var str2Total = 0;
+	
+	for(var i = 0; i < str1.length; i++) {
+		str1Total+=str1.charCodeAt(i)
+	}
+	for(var x = 0; x< str2.length; x++) {
+		str2Total += str2.charCodeAt(x)
+	}
+	if(str2Total === str1Total) {
+		return true;
+	} else {
+		return false;
+	}
 }
+rotationChecker("hello", "lloh");
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
