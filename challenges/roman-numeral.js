aab7499a801ffdcd5b6b3ebb736421611bffda20
binh.x.nguyen <binhxn89@gmail.com>
2016-07-07 09:56:47 -0700

    roman numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,40 @@
  */
 
 function romanNumeral(n) {
+	let conversions = {
+		I:1,
+		IV:4,
+		V:5,
+		IX:9,
+		X:10,
+		XL:40,
+		L:50,
+		XC:90,
+		C:100,
+		CD:400,
+		D:500,
+		CM:900,
+		M:1000
+	},
+		convertString = '';
 
+	for (let key in conversions) {
+		// key : conversions[key]
+		//   X : 10
+		
+		// if n is not in list, add i to convertString 
+		// and increase value by 1
+
+		// then decrease the conversions[key] value by
+		// the number that was just converted
+
+		while (n < conversions[key]) {
+			convertString += key;
+			n += conversions[key];
+		}		
+	}
+
+	console.log(convertString);
 }
 
 module.exports = romanNumeral;
