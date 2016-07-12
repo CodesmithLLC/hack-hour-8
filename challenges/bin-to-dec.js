004fd9bcfac7b493f31e4642118db12b94bf73cd
codesmith <student@mac-mini-6.local>
2016-07-12 10:01:29 -0700

    binToDec done, need to refactor decToBin to eliminate leading zeros

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,30 @@
  */
 
 function binToDec(binary) {
+	var binArr = binary.split('').reverse();
+	console.log(binArr);
+	return binArr.reduce(function(prev, curr, index){
+		if(curr === '1') return prev += Math.pow(2, index);
+		else return prev;
+	},0)
+}
 
+function decToBin(dec){
+	if (dec === 1) return '1';
+	var bin = '';
+	var binSpaces = Math.floor(Math.pow(dec));
+	console.log(binSpaces);
+	while(binSpaces>=0){
+		if(dec >= Math.pow(2, binSpaces)){
+			bin += '1';
+			dec -= Math.pow(2,binSpaces)
+		}
+		else{
+			bin+='0'
+		}
+		binSpaces--
+	}
+	return bin;
 }
 
 module.exports = binToDec;
