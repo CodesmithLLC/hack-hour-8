ed4f06d7bedc2d293c47b6cbc29f22317179ccd7
Blake Watkins <iamsoblake@gmail.com>
2016-07-12 10:36:32 -0700

    finished the first part

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -12,9 +12,28 @@
  * Extension:
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
+function log(it){
+  console.log(it);
+}
+
+function pow(base, power){
+  if(power === 0){
+    return 1;
+  }
+  return base * pow(base, power - 1);
+}
 
 function binToDec(binary) {
+  var binArr = binary.slice().split('').reverse();
+  var num = 0;
+  binArr.forEach(function(ele, idx){
+    if(ele === '1'){
+      num += pow(2, idx); 
+    }
+  })
 
+  return num;
 }
+
 
 module.exports = binToDec;
