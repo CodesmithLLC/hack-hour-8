8c01efd30b793fed264fdfaef84acf15aa3dcb87
Zhiwen Huang <zhiwen555@gmail.com>
2016-07-14 09:59:55 -0700

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,38 @@
  */
 
 function highestProduct(array) {
-
+  var max = 0;
+  var cache = {};
+  function recursive(arr, count, extract) {
+    if (count >= 2) {
+      for (var j = 0; j < arr.length; j++) {
+        var product = arr[j] * extract;
+        if (product > max) {
+          max = product;
+        }
+      }
+      return;
+    } else {
+      for (var i = 0; i < arr.length; i++) {
+        var arr2 = arr.slice()
+        var extract2 = arr2.splice(i, 1);
+        extract2 = extract * extract2[0];
+        var count2 = count + 1;
+        recursive(arr2, count2, extract2);
+      }
+    }
+  }
+  recursive(array, 0, 1);
+  return max;
 }
 
+console.log(highestProduct([1,2,3,8,2,3,4,100]))
+// console.log(highestProduct([5,2,8,1,7,2]))
+// console.log(highestProduct([2,1,2,9,1,2]))
+// console.log(highestProduct([1,2,3]))
+// console.log(highestProduct())
+// console.log(highestProduct())
+console.log(8*4*100)
 
-module.exports = highestProduct;
+
+// module.exports = highestProduct;
