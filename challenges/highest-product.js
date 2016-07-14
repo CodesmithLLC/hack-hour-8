b5dc9a1c668c22f94d6dd76516fa8d9be676cda3
Colin Brownlie <Colin Brownlie>
2016-07-14 14:47:21 -0700

    Okay, last attempt

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,16 +2,15 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
-  if (array.length < 3) throw new Error('Array must contain at least 3 integers');
-  let biggest = array[0] * array[1] * array[2];
-  for (let i = 0; i < array.length - 2; i++) {
-    const test = array[i] * array[i + 1] * array[i + 2];
-    if (test > biggest) {
-      biggest = test;
-    }
-  }
-  return biggest;
-}
+ function highestProduct(array) {
+   if (array.length < 3) throw new Error('Array must contain at least 3 integers');
+	 array.sort(function(a,b) {
+		 return a - b;
+	 });
+  var start = array[0] * array[1] * array[array.length - 1];
+ 	var end = array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
+ 	if (start > end) return start
+ 	else return end
+ }
 
 module.exports = highestProduct;
