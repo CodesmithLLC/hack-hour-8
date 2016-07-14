cdd3799f74452a09eab2ce4045901fc595492a28
campallison <allisonhanna@gmail.com>
2016-07-14 09:43:53 -0700

    working solution, as far as I can tell

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3) return false;
 
+  const sortArr = array.sort((a, b) => a - b);
+  const len = array.length;
+
+  let firstThreeMax = sortArr[0] * sortArr[1] * sortArr[2];
+  const lastThreeMax = sortArr[len - 1] * sortArr[len - 2] * sortArr[len - 3];
+
+  if (sortArr[0] < 0 && sortArr[1] < 0 && sortArr[2] < 0) {
+    firstThreeMax = sortArr[0] * sortArr[1] * sortArr[len - 1];
+  }
+
+  return Math.max(firstThreeMax, lastThreeMax);
 }
-
 
 module.exports = highestProduct;
