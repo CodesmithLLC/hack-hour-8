7295d2f3353f4d9412916050bbd95a41bb3b8c45
binh.x.nguyen <binhxn89@gmail.com>
2016-07-14 09:25:14 -0700

    highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,20 @@
  */
 
 function highestProduct(array) {
+	if (array.length < 3) {
+		console.log('Please enter an array with more than 3 integers.');
+		return;
+	}
 
+	// Sort array from highest to lowest
+	array.sort(function (a,b) {
+		return b-a;
+	});
+
+	let highestProduct = array[0] * array[1] * array[2];
+	
+	// console.log('highestProduct');
+	return highestProduct;
 }
 
 
