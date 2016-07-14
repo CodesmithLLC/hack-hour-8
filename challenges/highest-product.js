ecca36f7d1304ed7edcd946ccf422a65fcd683ad
Shray bansal <bansalshray@gmail.com>
2016-07-14 09:40:31 -0700

    highest product fixed

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,15 +2,16 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
- //if negative, then take two lowest numbers and 
+ //if negative, then take two lowest numbers and the highest
 	//brute force method
 
 function highestProduct(array) {
 	if (array.length <= 3) return array.reduce(function(curr, accum){return curr * accum; }, 1);
-	var prod;
+	var prod, prodPos;
 	array.sort(function(a,b){return b-a;});
 	prod = array[array.length-1] * array[array.length-2] * array[0];
-	if(prod <= 0) prod = array[0] * array[1] * array[2];
+	prodPos = array[0] * array[1] * array[2];
+	if (prodPos > prod) return prodPos
 	return prod;
 }
 
