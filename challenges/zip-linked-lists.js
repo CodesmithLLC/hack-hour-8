3b0c4be0becfff5b181b0518a0869a5a52f1af9d
campallison <allisonhanna@gmail.com>
2016-07-15 11:12:32 -0700

    eh maybe

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,22 @@ function Node(val) {
   this.next = null;
 }
 
+// I don't think this is right... I'm not sure if assigning the lists to nodes was correct.
 function zip(l1, l2) {
-};
+  const node1 = new Node(l1);
+  const node2 = new Node(l2);
 
-module.exports = {Node: Node, zip: zip};
+  // base cases: when all nodes from one LL is exhausted, just return the rest of the other
+  if (!node1) return node2;
+  if (!node2) return node1;
+
+  // use a recursive call to zip() as a way to flip between nodes from node1 and node2
+  // on each successive call, node1 and node2 switch places in the arguments list
+  // when we hit the base case (one of the parameters is null) we have the
+  // remainder of the other list from which the recursive calls will build the
+  // zipped list, in place, on the return value node1
+  node1.next = zip(node2, node1.next);
+  return node1;
+}
+
+module.exports = { Node: Node, zip: zip };
