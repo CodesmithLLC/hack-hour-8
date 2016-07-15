c411857d00c69227242d520f3db6dde302d2e502
Shray bansal <bansalshray@gmail.com>
2016-07-15 10:04:09 -0700

    zip linked list -not in place

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,14 +4,14 @@
 
  //if negative, then take two lowest numbers and the highest
 	//brute force method
-
+//O(n)
 function highestProduct(array) {
 	if (array.length <= 3) return array.reduce(function(curr, accum){return curr * accum; }, 1);
 	var prod, prodPos;
 	array.sort(function(a,b){return b-a;});
 	prod = array[array.length-1] * array[array.length-2] * array[0];
 	prodPos = array[0] * array[1] * array[2];
-	if (prodPos > prod) return prodPos
+	if (prodPos > prod) return prodPos;
 	return prod;
 }
 
