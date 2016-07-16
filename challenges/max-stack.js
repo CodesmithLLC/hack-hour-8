a2c88191ead9e0ebc1549ff4ce8b9443cd345e8e
Steven Harlan <SteveHarlan@Stevens-MacBook-Air.local>
2016-07-16 09:52:47 -0700

    completed max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,44 @@
  */
 
 function Stack() {
-  // body...
+  let arr;
+  let arrLength = 0;
+  let maxVal;
+  return {
+  "push" : function(value){
+    const arrAdd = new Array(arrLength + 1);
+    for (let i = 0; i < arrLength; i++) {
+      arrAdd[i] = arr[i];
+    }
+    arrAdd[arrLength] = value;
+    arr = arrAdd;
+    arrLength++;
+    // testing for Max with every push to keep O(1) of get max
+    if (arrLength === 1) {
+      maxVal = arr[0]
+    } else {
+      if (maxVal < value) {
+        maxVal = value;
+      }
+    }
+    return arrLength;
+  },
+  "pop": function() {
+    if (arrLength > 0) {
+      const arrPop = new Array(arrLength - 1);
+      const elRemove = arr[0];
+      for (let i = 1; i < arrLength; i++) {
+        for (let j = 0; j < arrLength - 1; j++) {
+          arrPop[j] = arr[i];
+        }
+      }
+      arrLength--;
+      arr = arrPop;
+      return elRemove;
+    }
+  },
+    "getMax": function() {return maxVal;}
+  };
 }
 
 module.exports = Stack;
