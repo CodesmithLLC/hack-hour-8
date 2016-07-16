6e59bc76ae95fb8a4ae5afb0cdb6bf008e49e610
jafow <jared.a.fowler@gmail.com>
2016-07-16 01:29:25 -0700

    highes product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,8 +2,21 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
+const negativesCount = (arr) => arr.reduce(
+    (ct, next) => next < 0
+    ? ct++
+    : ct
+    )
+
+
+
 function highestProduct(array) {
-  return 'definitely a test';
+  // sort by absolute val
+  var absValueSort = array.sort((a, b) => Math.abs(b) - Math.abs(a))
+
+  return negativesCount(absValueSort.slice(0, 3)) == 2
+    ? 'something'
+    : 'another thing'
 }
 
 
