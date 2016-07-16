a3a6576f17656602e63a1fead70b2d8596c07b3a
campallison <allisonhanna@gmail.com>
2016-07-16 09:45:22 -0700

    solution for max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,31 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.index = 0;
 }
+
+Stack.prototype.push = (value) => {
+  this.storage[this.index] = value;
+  this.index++;
+  return this.index;
+};
+
+Stack.prototype.pop = () => {
+  const len = Object.keys(this.storage).len;
+  if (len === 0) return undefined;
+  const lastIdx = len - 1;
+  const lastItem = this.storage[lastIdx];
+
+  delete this.storage[lastIdx];
+  return lastItem;
+};
+
+Stack.prototype.getMax = () => {
+  const vals = Object.values(this.storage);
+  const len = vals.length;
+  vals.sort((a, b) => a - b);
+  return vals[len - 1];
+};
 
 module.exports = Stack;
