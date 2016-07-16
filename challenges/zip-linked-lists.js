a8c6eae895af514b0f7a682f82a8dfc14d592d24
Yandri Sanchez <ysanchez@Yandris-MacBook-Pro.local>
2016-07-16 09:13:54 -0700

    --

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,51 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+
+l1.next = zip(l2, l1.next);
+return l1;
+  // if (l1.value === undefined) {
+  //   return l2;
+  // } else if (l2.value === undefined) {
+  //   return l1;
+  // }
+  
+  // var zipped
+  // var tail;
+  
+  // var l1ContainsItems = true;
+  // var l2ContainsItems = true;
+  // var l1tail = l1.next;
+  // var l2tail = l2.next
+  
+  // while (l1ContainsItems || l2ContainsItems) {
+  //   if (zipped === undefined) {
+  //     zipped = new Node(l1.value);
+  //     zipped.next = new Node(l2.value)
+  //     tail = zipped.next.next;
+  //   } else {
+      
+  //     if(l1tail) {
+  //       tail = new Node(l1.next.value);
+  //       tail = tail.next;
+  //       l1tail = l1tail.next;
+  //     } else {
+  //       l1ContainsItems = false;
+  //     }
+      
+  //     if(l2tail) {
+  //       tail = new Node(l2.next.value);
+  //       tail = tail.next;
+  //       l2tail = l2tail.next;
+  //     } else {
+  //       l2ContainsItems = false;
+  //     }
+      
+  //   }
+      
+  // }
+
+  // return zipped;
 };
 
 module.exports = {Node: Node, zip: zip};
