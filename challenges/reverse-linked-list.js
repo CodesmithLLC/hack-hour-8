ad7d0d6da1a7bc11c697b9fae0088fecb0080b31
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-18 10:00:35 -0700

    attempt 1

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,41 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
+  let tail = head;
+  let count = 0;
+  while (tail.next) {
+    tail = tail.next;
+    count++;
+  }
+  let newTail = head;
+  const newLL = new Node(tail.value);
+  let newNode = newLL;
+  while (count) {
+    for (let i = 0; i < count; i++) {
+      newTail = newTail.next;
+    }
+    newNode.next = new Node(newTail.value);
+    newNode = newNode.next;
+    count--;
+  }
+  return newLL;
+}
 
+function reverseLinkedListInplace(head) {
+  if (!head.next) return head;
+  const newHead = reverseLinkedList(head.next);
+  newHead.next = head;
+  return newHead;
+}
+
+function reverseLinkedListInplace2(head) {
+  if (!head.next) return head;
+  return reverseLinkedList2(head.next).next = head;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
