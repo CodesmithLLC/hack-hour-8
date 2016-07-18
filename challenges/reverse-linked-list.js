43c6e90c450dc922029d9c5e832733d143531ea8
Blake Watkins <iamsoblake@gmail.com>
2016-07-18 10:03:07 -0700

    tentative solution

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,21 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  var tempCurr = Object.assign({}, head);
 
+  ////////////Loop////////////
+  while(tempCurr.next != null){
+    head = tempCurr.next;
+    head.next = tempCurr;
+    tempCurr = tempCurr.next;
+  }
+  ////////////////////////////
+
+  //This seems wrong, another way I thought of doing this, though it's
+  //not that great on time complexity, is to loop through the list, and augment 
+  //nodes with 'prev' and a final 'tail' value, then start from tail of list, and 
+  //loop back up and start switching the 'next' and 'prev' properties.
+  return head;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
