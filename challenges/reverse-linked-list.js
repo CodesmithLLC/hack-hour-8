2fe92a3294b77d216a1bdb3b299a743145211749
Mike Puangmalai <mike@nongaap.com>
2016-07-18 09:44:22 -0700

    reverse linked list complete but not done in place

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -8,13 +8,36 @@
  *
  */
 
+ /*
+Initial Approach:
+1. Loop through the linked list using while putting each object in an array using unshift.
+2. Go through array using a for loop and change this.next to 'i+1' object.
+3. Return new head with reversed linkedlist
+
+
+ */
+
 function Node(value) {
     this.value = value;
     this.next = null;
 }
 
 function reverseLinkedList(head) {
+  var container = [];
+  var node = head;
+  
+  while(node){
+  	container.unshift(node);
+  	node = node.next;
+  }
+
+  for(var i = 0; i < container.length-1; i++) {
+  	container[i].next = container[i+1];
+  }
+
+  return container[0];
 
 }
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
