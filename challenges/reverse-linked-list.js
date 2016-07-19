801327799221578fa0b3d22f937cc5f431aba13f
Micky Shaked <micky.shaked@gmail.com>
2016-07-19 10:06:31 -0700

    stocks almost

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -11,10 +11,40 @@
 function Node(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
 }
 
-function reverseLinkedList(head) {
-
+function reverseLinkedList(head){
+	var currentNode = head;
+	// traverse to end of list
+	while (currentNode.next){
+		var temp = currentNode;
+		currentNode = currentNode.next;
+		currentNode.prev = temp;
+	}
+	// set head of new list to end of original list
+	var newHead = new Node(currentNode.value);
+	newHead.next = currentNode.prev;
+	currentNode = currentNode.prev;
+	// traverse backwards through original list, adding each node to new list
+	while(currentNode.value){
+		newHead.next = currentNode;
+		newHead = currentNode;
+		// var newNode = new Node(currentNode.value);
+		// newNode.next = currentNode.prev;
+		currentNode = currentNode.prev;
+	}
+	return newHead.value;
 }
+
+var head = new Node(1);
+var node1 = new Node(2);
+head.next = node1;
+node1.prev = head;
+var node2 = new Node(3);
+node1.next = node2;
+node2.prev = node1;
+console.log(node2);
+reverseLinkedList(head);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
