e4394c2e55686b0c2e961d3b5ff8d853349b649c
davidvorona <davidavorona@gmail.com>
2016-07-20 09:33:56 -0700

    refactored zip and reverse, finished arraysort

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,43 +14,16 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-	var curr = head;
-	var tail = head;
-	var startVal = head.value;
-	while (true) {
-		if (curr.next === null) {
-			node = new Node(curr.value);
-			while (true) {
-				if (endVal !== startVal) {
-					if (tail.value === endVal) {
-						console.log(node);
-						console.log(tail);
-						node.next = tail;
-						tail = head;
-						console.log(tail);
-						console.log(tail.value);
-						console.log(endVal);
-						console.log(node);
-						endVal = // the new tail value!
-						count--;
-					} else {
-						tail = tail.next;
-						if (tail.value === endVal) tail.next = null;
-					}
-				} else {
-					return node;
-				}
-			}
-		} else {
-			if (curr.next.next === null) {
-				var endVal = curr.value;
-			}
-			curr = curr.next;
-		}
-	}
+  prev = null
+  current = head
+  while(current) {
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+    head = prev
+  }
+  return head;
 }
-
-// var link = {value: 2, next: {value: 3, next: {value: 4, next: {value: 5, next: null}}}}
-// reverseLinkedList(link);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
