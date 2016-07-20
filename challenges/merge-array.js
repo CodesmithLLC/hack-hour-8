e298319d1f8861f62cf43f2c10461e60a26ab3a3
Colin Brownlie <Colin Brownlie>
2016-07-20 09:58:57 -0700

    not sure on the space complexity

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,36 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time and O(n) space 
+ *
+ * BONUS:
+ * Complete in O(n) time and O(n) space
  *
  */
 
 function mergeArrays(arr1, arr2) {
-
+  let i = 0;
+  let j = 0;
+  const res = [];
+  while (res.length < arr1.length + arr2.length) {
+    if (j === arr2.length) {
+      res.push(arr1[i]);
+      i++;
+    }
+    else if (i === arr1.length) {
+      res.push(arr2[j]);
+      j++;
+    }
+    else if (arr1[i] <= arr2[j]) {
+      res.push(arr1[i]);
+      i++;
+    }
+    else if (arr1[i] > arr2[j]) {
+      res.push(arr2[j]);
+      j++;
+    }
+  }
+  return res;
 }
+
 
 module.exports = mergeArrays;
