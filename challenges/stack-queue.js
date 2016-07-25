9f5b37365611447898923aacc373a28da84c4ecd
campallison <allisonhanna@gmail.com>
2016-07-25 09:30:34 -0700

    I think this works

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,39 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
-
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
 }
-
 
 /**
 * Queue Class
 */
-
-
 function Queue() {
+  this.storage = {};
+  this.index = 0;
+  const stack = new Stack();
+  let i = 0;
 
+  while (i < 2) {
+    this.storage[this.index] = stack;
+    this.index++;
+    i++;
+  }
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+// function enqueue(value) {
+//   this.storage[this.index] = value;
+//   this.index++;
+//   return this.index;
+// }
+//
+// function twoStackQueue() {
+//   const q = new Queue();
+//   q.enqueue(stack);
+//   q.enqueue(stack);
+//
+//   return q;
+// }
+
+module.exports = { Stack: Stack, Queue: Queue };
