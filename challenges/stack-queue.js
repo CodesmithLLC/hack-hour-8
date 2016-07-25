1cbc2aaf15a68cd9d2ee15a04546fc92f8ec001e
Cole Williams <cowi4030@colorado.edu>
2016-07-25 13:33:04 -0700

    stack and queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,25 @@
 
 
 function Stack() {
+	this.storage = [];
+	
+	var index = 0;
 
 }
 
+Stack.prototype.push = function(item) {
+		this.storage[index] = item
+		index++
+}
+
+Stack.prototype.pop = function(item) {
+	var deleted = remove this.storage[index - 1]
+	return deleted
+}
+
+Stack.prototype.size = function() {
+	return this.storage.length
+}
 
 /**
 * Queue Class
@@ -14,7 +30,21 @@ function Stack() {
 
 
 function Queue() {
+	var inbox = new Stack();
+	var outbox = new Stack();
+}
 
+Queue.prototype.enque = function(item) {
+	inbox.push(item)
+}
+
+Queue.prototype.dequeue = function(item) {
+	if(outbox.size() === 0) {
+		while(inbox.size()) {
+			outbox.push(inbox.pop());
+		}
+		return outbox.pop();
+	}
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
