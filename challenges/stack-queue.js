23db85ed3ab4a801683b39b45dec7221f05f86d8
Morgan <morgan.christison@gmail.com>
2016-07-25 09:59:09 -0700

    queue two stacks

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,35 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
-
 function Stack() {
-
+	this.storage = [];
 }
-
-
+Stack.prototype.push(value){
+	this.storage.push(value);
+};
+Stack.prototype.pop(){
+	return this.storage.pop();
+};
 /**
 * Queue Class
 */
-
-
 function Queue() {
-
+	this.stack1 = [];
+	this.stack2 = [];
 }
+Queue.prototype.enqueue = function(value) {
+	this.stack1.push(value);
+};
+Queue.prototype.dequeue = function() {
+	if(!this.stack2.length){
+		if(!this.stack1.length){
+			return undefined;
+		}
+		while(this.stack1.length){
+			this.stack2.push(this.stack1.pop());
+		}
+	}
+	return this.stack2.pop();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
