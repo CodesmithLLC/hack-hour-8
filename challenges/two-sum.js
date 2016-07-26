8a905dc87494e2e183c080caa736b7d3701be799
binh.x.nguyen <binhxn89@gmail.com>
2016-07-26 10:04:51 -0700

    two sum hack hour

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,30 @@
  */
 
 function twoSum(arr, n) {
+	// Sorted array
+	arr.sort((a,b) => { return a - b })
+	
+	// Test if arr can be added
+	if (arr === null || arr.length === 0) return false
 
+	let i = 0;
+  let j = arr.length - 1;
+
+  while (i < j) {
+    let x = arr[i] + arr[j];
+
+    // If the array sum is less than target, increase
+    // index from start of array
+    if (x < n) { ++i }
+
+    // If the array sum is less than target, decrease
+    // index from end of array
+    else if (x > n) { j-- }
+    	
+    else { return true }
+  }
+  
+  return false;
 }
 
 module.exports = twoSum;
