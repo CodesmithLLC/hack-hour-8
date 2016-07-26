c582d5dac4b944a9daaed3dad81c5fc007106eeb
Blake Watkins <iamsoblake@gmail.com>
2016-07-26 09:54:31 -0700

    seems to accommodate edge cases

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+  for(var i = 0; i < arr.length; i++){
+    var idx = arr.indexOf(n - arr[i]);
+    var lastIdx = arr.lastIndexOf(n - arr[i]);
+    if((idx > -1 || lastIdx > -1) && (idx != i && lastIdx != i)) return true;
+  }
+  return false;
 }
 
+// var array = [3,6,7,2,9];
+// var num = 15;
+// console.log(twoSum(array, num));
 module.exports = twoSum;
