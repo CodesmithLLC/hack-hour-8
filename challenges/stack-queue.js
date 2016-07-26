80a3a513297bd7e88400e14308890c1b7d89b726
kevinl <kevinliu987@gmail.com>
2016-07-25 22:29:22 -0700

    double stack

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -3,18 +3,45 @@
  */
 
 
-function Stack() {
+ function Stack() {
+   this.storage = {};
+   this.index = 0;
+   this.push = function(value) {
+   	this.storage[this.index]=value;
+   	this.index++;
+   	return this.index;
+   };
+   this.pop = function() {
+   	let popped = this.storage[this.index -1];
+   	delete this.storage[this.index -1];
+   	this.index--;
+   	return popped;
+   }
+ }
 
-}
 
 
-/**
-* Queue Class
-*/
+ /**
+ * Queue Class
+ */
 
 
-function Queue() {
+ function Queue() {
+ 	this.inbox = new Stack();
+ 	this.outbox = new Stack();
+ 	this.push = function(value) {
+ 		 return this.inbox.push(value);
+ 	};
 
-}
+ 	this.pop = function() {
+ 		if (!this.outbox.index && !this.inbox.index) {return 'empty queue'};
+ 		while (this.inbox.index){
+ 			this.outbox.push(this.inbox.pop());
+ 		}
+ 		return this.outbox.pop();
+ 	}
+
+ }
+
 
 module.exports = {Stack: Stack, Queue: Queue};
