3eeea2a548688516c3c2ac5e97c11c654f9599ec
Mike Puangmalai <mike@nongaap.com>
2016-07-27 10:04:10 -0700

    valid bst not finished. will revisit the problem later

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,6 +4,11 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
  
+ /*
+  Initial approach. Write a function that evaluates a BST and return true if it's valid or false if it does not meet the rules of a valid BST.
+
+  The function will need to 1) traverse the tree and 2) compare the value of left and right objects vs. the object's value. Task #1 will likely require a while loop to traverse. Task #2 could be a helper comparison function. The comparison helper function could potentially be called recursively.
+ */
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,7 +17,30 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
-
+  // console.log(tree);
+  // while(tree.left) {
+  // //	if(tree.value < tree.left.value || tree.value >= tree.right.value) {return false}
+  // 	validBST(tree.left);
+  // //  validBST(tree.right);
+  // }
+  // return true;
 }
+
+// var test1 = new BinaryTree(3);
+// var test2 = new BinaryTree(2);
+// var test3 = new BinaryTree(4);
+// var test4 = new BinaryTree(1);
+// var test5 = new BinaryTree(3);
+// var test6 = new BinaryTree(3);
+// var test7 = new BinaryTree(5);
+
+// test1.left = test2;
+// test1.right = test3;
+// test2.left = test4
+// test2.right = test5
+// test3.left = test6
+// test3.right =test7
+
+// validBST(test1);
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
