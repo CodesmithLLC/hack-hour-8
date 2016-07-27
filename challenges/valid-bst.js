9089350c690987d544a27878be3e587b6f6087a5
Micky Shaked <micky.shaked@gmail.com>
2016-07-27 10:03:37 -0700

    bin tree base case not being triggered

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,7 +12,37 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
-
+	var current = tree;
+	return check(current);
+	function check(node){
+		// console.log(node.value,node.left.value,node.right.value);
+		if(node.left === null && node.right === null) return true;
+		if(node.value >= node.left.value){
+			console.log('node left to check ',node.left);
+			check(node.left);
+		}
+		if(node.value < node.right.value){
+			console.log('node ', node.value, node.right.value);
+			console.log('node right to check ',node.right);
+			check(node.right);
+		}
+		return false;
+	}
 }
+
+var binTree = new BinaryTree(10)
+var binTree1 = new BinaryTree(9);
+var binTree2 = new BinaryTree(11);
+var binTree3 = new BinaryTree(8);
+var binTree4 = new BinaryTree(10);
+var binTree5 = new BinaryTree(10);
+var binTree6 = new BinaryTree(8);
+binTree.left = binTree1;
+binTree.right = binTree2;
+binTree1.left = binTree3;
+binTree1.right = binTree4;
+binTree2.left = binTree5;
+binTree2.right = binTree6;
+console.log(validBST(binTree));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
