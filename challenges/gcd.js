dec5fb52e4aa37f63cdc47147704343e02ace9c1
binh.x.nguyen <binhxn89@gmail.com>
2016-07-28 10:03:59 -0700

    gcd hack hour

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,20 @@
  */
 
 function gcd(a, b) {
+ 	if (a === 0) {
+    return b;
+ 	}
 
+  while (b !== 0) {
+    if (a > b) {
+      a = a - b;
+    }
+    else {
+      b = b - a;
+    }
+  }
+
+  return a;
 }
 
 module.exports = gcd;
