8b5ce81337f81db47062a3e76d1455869388bd02
Micky Shaked <micky.shaked@gmail.com>
2016-07-28 08:50:45 -0700

    reverse linked list completed (with help from the internet)

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -11,40 +11,25 @@
 function Node(value) {
     this.value = value;
     this.next = null;
-    this.prev = null;
 }
 
 function reverseLinkedList(head){
-	var currentNode = head;
-	// traverse to end of list
-	while (currentNode.next){
-		var temp = currentNode;
-		currentNode = currentNode.next;
-		currentNode.prev = temp;
+	var node = head;
+	var previous = null;
+	while(node){
+		var save = node.next;
+		node.next = previous;
+		previous = node;
+		node = save;
 	}
-	// set head of new list to end of original list
-	var newHead = new Node(currentNode.value);
-	newHead.next = currentNode.prev;
-	currentNode = currentNode.prev;
-	// traverse backwards through original list, adding each node to new list
-	while(currentNode.value){
-		newHead.next = currentNode;
-		newHead = currentNode;
-		// var newNode = new Node(currentNode.value);
-		// newNode.next = currentNode.prev;
-		currentNode = currentNode.prev;
-	}
-	return newHead.value;
+	return previous;
 }
 
 var head = new Node(1);
 var node1 = new Node(2);
 head.next = node1;
-node1.prev = head;
 var node2 = new Node(3);
 node1.next = node2;
-node2.prev = node1;
-console.log(node2);
 reverseLinkedList(head);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
