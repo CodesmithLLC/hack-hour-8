477c610ad3728600e0f51bee7b55c81806a5957a
davidvorona <davidavorona@gmail.com>
2016-07-28 09:46:56 -0700

    finished hack-hour

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,14 @@
  */
 
 function gcd(a, b) {
-
+  if (a === b) return a;
+  if (a > b) {
+    if ((a % b) === 0) return b;
+    else return gcd((a % b), b);
+  } else {
+    if ((b % a) === 0) return a;
+    else return gcd((b % a), a);
+  }
 }
 
 module.exports = gcd;
