c2b457f1cc22a8b521890f301f1c97d509f59d25
Mike Puangmalai <mike@nongaap.com>
2016-07-28 10:05:36 -0700

    gcd complete

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,22 @@
  *
  */
 
+ /*
+Edge cases to consider:
+  Negative numbers.
+  Two Zeros.
+  One Zero.
+ */
+
 function gcd(a, b) {
+  if(a === 0 && b ===0) return undefined;
+  if(a === 0 || b=== 0) return Math.max(Math.abs(a),Math.abs(b));
+  var absoluteMaxSquare = Math.floor(Math.sqrt(Math.max(Math.abs(a),Math.abs(b))));
+  for (var i = absoluteMaxSquare; i>0; i--) {
+  	if(a % i === 0 && b % i === 0 ) return i;
+  }
 
 }
+
 
 module.exports = gcd;
