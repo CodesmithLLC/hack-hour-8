15fddaadc86ff50d22a9981a5ecda6923e6ae60f
Chandra Lindy <chandra.lindy@gmail.com>
2016-07-29 09:52:06 -0700

    attempt 1

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,7 +26,20 @@
  */
 
 function applyIt(func, args) {
+  const obj = {};
+  args.forEach((el, i) => {
+     obj[i] = el;
+  });
 
+  return function () {
+    return func(obj[0], obj[1], obj[2], obj[3], obj[4], obj[5],
+                obj[6], obj[7], obj[8], obj[9], obj[10], obj[11],
+                obj[12], obj[13], obj[14], obj[15], obj[16], obj[17],
+                obj[18], obj[19], obj[20], obj[21], obj[22], obj[23],
+                obj[24], obj[25], obj[26], obj[27], obj[28], obj[29],
+                obj[30], obj[31], obj[32], obj[33], obj[34], obj[35]
+            );
+  };
 }
 
 module.exports = applyIt;
