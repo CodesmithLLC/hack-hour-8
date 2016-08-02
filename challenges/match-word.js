868ca5dfe461db9979e9c0a0ed789458b29f736a
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-02 15:35:02 -0700

    fix match-word for nested inputs

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,4 +1,8 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets.
+// Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem
+// we'll check that all words in a string are "closed". Write a function that takes a
+// string and returns true if every word is closed by its backwards counterpart. Words
+// must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
@@ -13,6 +17,20 @@ function matchWord(str) {
     return false;
   }
 
+  function checkPit(word, pit) {
+    const lastIndex = pit.length - 1;
+    const reversed = word.split('').reverse().join('');
+    if (pit[lastIndex] !== word) {
+      if (pit[lastIndex] === reversed) pit.pop();
+      else {
+        pit.push(word);
+      }
+    }
+    else {
+      pit.push(word);
+    }
+  }
+
   if (str === '') return true;
   const input = str.toLowerCase();
   const pit = [];
@@ -24,17 +42,13 @@ function matchWord(str) {
         word = word + input[i];
       } else {
         word = word + input[i];
-        const lastIndex = pit.length - 1;
-        const reversed = word.split('').reverse().join('');
-        if (pit[lastIndex] === reversed) pit.pop();
-        else {
-          pit.push(word);
-          word = '';
-        }
+        checkPit(word, pit);
+        word = '';
       }
     }
   }
 
+  checkPit(word, pit);
   if (pit.length) return false;
   return true;
 }
