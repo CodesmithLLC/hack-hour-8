153c352ecf7dbb6dfe2e75ac4ab2a70f4b15ee5a
campallison <allisonhanna@gmail.com>
2016-08-08 09:26:41 -0700

    working solution, brute forcey

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,14 +1,27 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 2222 counts as having 4 2s.
+// Write a function "countTwos" that takes a number as an argument, n. The
+// function will return the number of 2s encountered when counting from 1 to n
+// (inclusive). 2222 counts as having 4 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
 // countTwos(13);  -> 2
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
-
+/*
+could create an array of all the numbers,
+then for each,
+array[i].toString, match(/[2]/gm)
+*/
 
 function countTwos(num) {
+  const numArr = [];
+  let i;
 
+  for (i = 1; i <= num; i++) {
+    numArr.push(i);
+  }
+
+  return numArr.join('').match(/[2]/gm).length;
 }
 
 module.exports = countTwos;
