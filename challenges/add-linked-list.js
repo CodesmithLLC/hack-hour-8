adfcfac5344e55173e6bbbb7fe8674d6656dbeba
Blake Watkins <iamsoblake@gmail.com>
2016-08-09 10:09:20 -0700

    interesting problem

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -8,13 +8,60 @@
  *
  */
 
-function Node(val) {
+function node(val) {
   this.value = val;
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
+function flattenlist(list){
+  var current = list;
+  var numstr = '';
+  while(typeof current.value === 'number'){
+    numstr = current.value + numstr;
+    current = current.next;
+  }
+  return +numstr;
+}
 
+function singlist() {
+    this._length = 0;
+    this.head = null;
+}
+
+singlist.prototype.add = function(value) {
+    var node = new node(value),
+        currentnode = this.head;
+ 
+    // 1st use-case: an empty list 
+    if (!currentnode) {
+        this.head = node;
+        this._length++;
+         
+        return node;
+    }
+ 
+    // 2nd use-case: a non-empty list
+    while (currentnode.next) {
+        currentnode = currentnode.next;
+    }
+ 
+    currentnode.next = node;
+ 
+    this._length++;
+     
+    return node;
+};
+
+function addlinkedlist(l1, l2) {
+
+  var sum = flattenlist(l1) + flattenlist(l2);
+  var revarr = sum.tostring.split('').reverse();
+  var list = new singlist();
+  for(var i = 0; i < revarr; i++){
+    list.add(+revarr[i]);
+  }
+
+  return list;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
