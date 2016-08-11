34a88027eba0708a5bd29f58d0055f05ff2c1f81
Mike Puangmalai <mike@nongaap.com>
2016-08-11 09:17:09 -0700

    repeat number complete

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,12 @@
  */
 
 function repeatNumbers(array) {
-
+  for(var i = 0; i < array.length; i++){
+  	if(array.indexOf(array[i]) !== i ) {
+  		return array[i];
+  	}
+  }  
 }
+
 
 module.exports = repeatNumbers;
