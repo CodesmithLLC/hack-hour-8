6538583e44ceac05d5771de6f2bbd3d41aeb5ba1
Mike Puangmalai <mike@nongaap.com>
2016-08-12 12:46:00 -0700

    merge ranges done

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,41 @@
  * Do not assume the ranges are in order
  */
 
-
 function mergeRanges(array) {
+  var output = [];
+  var reducedArray = [];
+
+  var sortedArray = array.sort(function (a, b) {
+  	if (a[0] === b[0]) {
+  		return 0;
+  	}
+  	else {
+  		return (a[0] < b[0]) ? -1 : 1;
+  	}
+  });
+
+  sortedArray.reduce(function(a,b,i){
+  	if(i === sortedArray.length-1) {
+  		return reducedArray.push(a.concat(b));
+  	}
+  	else if(a[a.length-1] >= b[0]){
+  		return a.concat(b);
+  	} else {
+  		reducedArray.push(a);
+  		return [].concat(b);
+  	}
+  });
+
+  reducedArray.forEach(function(value){
+  	var temp = [];
+  	temp.push(value[0]);
+  	temp.push(value[value.length-1]);
+  	output.push(temp);
+  });
+
+  return output;
 
 }
+
 
 module.exports = mergeRanges;
