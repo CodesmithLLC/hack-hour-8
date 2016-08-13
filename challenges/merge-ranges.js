ef9a9ba450f3b5e286ee9ec733e86fd80b20e007
campallison <allisonhanna@gmail.com>
2016-08-13 09:37:22 -0700

    just stashing

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -1,5 +1,6 @@
 /**
- * Write a function mergeRanges that takes an array of meeting time ranges and returns an array of condensed ranges.
+ * Write a function mergeRanges that takes an array of meeting time ranges and
+ * returns an array of condensed ranges.
  *
  * Example:
  * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]
@@ -8,7 +9,6 @@
  *
  * Do not assume the ranges are in order
  */
-
 
 function mergeRanges(array) {
 
