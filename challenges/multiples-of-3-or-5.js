a0f36495dd0a95f5a1551d839947b1ca4d38d5b2
campallison <allisonhanna@gmail.com>
2016-08-13 09:47:06 -0700

    brute force solutions

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -1,26 +1,41 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
-//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 // write a function that will find the sum of all the multiples of 3 or 5
-//below 1000 and return that sum.
+// below 1000 and return that sum.
 
 function sumMultiples3Or5Below1000() {
-  var sum = 0;
+  let sum = 0;
+  let i;
+
+  for (i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
 
   return sum;
 }
 
-//extension make it dynamic function that takes input x,y,z
-//and returns the sum of multiples of x and y below z
-function sumMultiplesXOrYBelowZ(x,y,z) {
-  var sum = 0;
+// extension make it dynamic function that takes input x,y,z
+// and returns the sum of multiples of x and y below z
+
+function sumMultiplesXOrYBelowZ(x, y, z) {
+  let sum = 0;
+  let i;
+
+  for (i = 0; i < z; i++) {
+    if (i % x === 0 || i % y === 0) {
+      sum += i;
+    }
+  }
 
   return sum;
 }
 
-var objectToExport={
-  sumMultiples3Or5Below1000:sumMultiples3Or5Below1000,
-  sumMultiplesXOrYBelowZ:sumMultiplesXOrYBelowZ
+var objectToExport = {
+  sumMultiples3Or5Below1000: sumMultiples3Or5Below1000,
+  sumMultiplesXOrYBelowZ: sumMultiplesXOrYBelowZ,
 };
 
 module.exports = objectToExport;
