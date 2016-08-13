ee27aceddd77b38bca74f673fab42de64a118851
Blake Watkins <iamsoblake@gmail.com>
2016-08-13 09:06:04 -0700

    need to sort values in mergeranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,22 @@
 
 
 function mergeRanges(array) {
-
+  //need to sort array first, will do...
+  var result = [];
+  for(var i = 0; i < array.length; i++){
+    if(array[i][array[i].length - 1] > array[i + 1][0]){
+      var catted = array[i].concat(array[i + 1]);
+      result.push([Math.min(...catted), Math.max(...catted)])
+      i++;
+    }else{
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
+
+// var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+
+// console.log(mergeRanges(times)); // -> [[0, 1], [3, 8], [9, 12]]
 
 module.exports = mergeRanges;
