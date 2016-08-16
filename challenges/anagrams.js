57c15f7ca08bc345bb3fd267f14f6811ddad8e94
Mike Puangmalai <mike@nongaap.com>
2016-08-16 10:50:10 -0700

    anagram not complete

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,26 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+function looper(index, array) {
+  if(index > 0) {
+  	return array.slice(index).concat(array.slice(0,index));
+  }
+  return array;
+}
+
 function anagrams(string) {
+  var output = [];
+  var arr = string.split('');
+  for(var i = 0; i < arr.length; i++){
+  	
+  }
 
 }
+
+var result = anagrams('abc');
+//console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+console.log(looper(0,['a','b','c']));
+console.log(looper(1,['a','b','c']));
+console.log(looper(2,['a','b','c']));
 
 module.exports = anagrams;
