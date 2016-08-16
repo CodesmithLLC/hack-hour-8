37f1a2622c43ad6f884f5accdf484649179b9210
campallison <allisonhanna@gmail.com>
2016-08-16 09:35:42 -0700

    nowhere near right, just stashing

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,30 @@
   */
 
 function anagrams(string) {
+  if (typeof string !== 'string' || string.length === 0) return false;
 
+  const len = string.length;
+  const res = [];
+  const stringRes = '';
+  let i;
+  let j;
+  const letters = string.split('');
+
+  const helper = (len, str) => {
+    if (len === 0) {
+      return stringRes.concat(str);
+    }
+
+    for (i = 0; i < len; i++) {
+      helper(len - 1, str.concat(str));
+    }
+  };
+
+  for (j = 0; j < letters.length; j++) {
+    res.push(helper(len, letters[j]));
+  }
+
+  return res;
 }
 
 module.exports = anagrams;
