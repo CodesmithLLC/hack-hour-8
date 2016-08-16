a4afa0b73ac71d3c1bd443bf5057e7052f4055c2
Shray bansal <bansalshray@gmail.com>
2016-08-16 10:01:58 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,24 @@
   */
 
 function anagrams(string) {
-
+    function recursive(string, prefix) {
+        if (string.length === 0) {
+            return [prefix];
+        } else {
+            var out = [];
+            for (var i = 0; i < string.length; i++) {
+                var pre = string.substring(0, i);
+                var post = string.substring(i + 1);
+                out = out.concat(recursive(pre + post, string[i] + prefix));
+            }
+            return out;
+        }
+    }
+    var distinct = {};
+    recursive(string, "").forEach(function(result) {
+        distinct[result] = true;
+    });
+    return Object.keys(distinct);
 }
 
 module.exports = anagrams;
