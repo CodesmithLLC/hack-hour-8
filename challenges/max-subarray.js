2da396b40df3105891509a555b5bad98510823db
jafow <jared.a.fowler@gmail.com>
2016-08-16 20:50:21 -0700

    max array sub

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,14 @@
  */
 
 function maxSubarray(arr) {
+  var currentMax = Number.NEGATIVE_INFINITY;
+  var finalMax = Number.NEGATIVE_INFINITY;
 
+  for (var i = 0; i < arr.length; i++) {
+    currentMax = Math.max(arr[i], currentMax + arr[i]);
+    finalMax = Math.max(finalMax, currentMax);
+  }
+  return finalMax;
 }
 
 module.exports = maxSubarray;
