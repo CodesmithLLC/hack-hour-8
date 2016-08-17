401a0572d55d25e78a00a242c500a011385dd7b2
Mike Puangmalai <mike@nongaap.com>
2016-08-17 10:26:38 -0700

    delete dups complete

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,28 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+/*
+Approach: Traverse linked list and track values. If there's a duplicate, unlink the obect and link next to the following object.
 
+*/
 
 function deleteDups(head) {
+  var values = [];
+  var obj = head;
+  var prevObj;
 
+  while (obj) {
+  	if(values.indexOf(obj.value) < 0) {
+  		values.push(obj.value);
+  		prevObj = obj;
+  		obj = obj.next;
+  	} else {
+  		prevObj.next = obj.next;
+  		obj = obj.next;
+  	}
+  }
+  
+  return head;
 }
 
 module.exports = deleteDups;
