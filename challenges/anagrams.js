d57bb703228d9bffebd2e16f143fadcd635741e5
Yandri Sanchez <ysanchez@Yandris-MacBook-Pro.local>
2016-08-17 11:34:38 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -1,4 +1,4 @@
-/**
+5/**
  * Given a single input string, write a function that produces all possible anagrams
  * of a string and outputs them as an array. At first, don't worry about
  * repeated strings.  What time complexity is your solution?
@@ -13,7 +13,26 @@
   */
 
 function anagrams(string) {
+  let result = [];
 
+  for (let i = 0; i < string.length; i++) {
+    let current = string[i];
+    let left = string.slice(0, i);
+    let right = string.slice(i + 1, string.length);
+
+    let extra = right + left;
+    result.push(current + extra);
+    
+    for (let n = 1; n < extra.length; n++) {
+      let curr = extra[n];
+      let prev = extra.slice(0, n);
+      let next = extra.slice(i + 1, string.length);
+
+      result.push(current + curr)
+    }
+  }
+
+  console.log(result);
 }
 
 module.exports = anagrams;
