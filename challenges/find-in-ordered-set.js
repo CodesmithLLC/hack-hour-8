683502d4d37f8bd033b57d72c7a69cff5ea84f11
Micky Shaked <micky.shaked@gmail.com>
2016-08-19 09:37:28 -0700

    find in ordered set with recursion works

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,22 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+	//base case target has been found
+	if(target === arr[0]) return true;
 
+	//base case target is not in array
+	if(arr.length === 1) return false;
+
+	//split array into two sub arrays
+	var arr1 = arr.slice(0, Math.floor(arr.length/2));
+	var arr2 = arr.slice(Math.floor(arr.length/2));
+
+	//check if target is at end of 1st half or beginning of 2nd half
+	if(target === arr1[arr1.length-1] || target === arr2[arr2.length-1]) return true;
+
+	//check if target would be in first half or 2nd half and recurse
+	if(target < arr1[arr1.length-1]) return findInOrderedSet(arr1, target);
+	else return findInOrderedSet(arr2,target);
 }
-
 
 module.exports = findInOrderedSet;
