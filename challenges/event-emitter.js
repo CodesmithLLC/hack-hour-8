bab5437d06b05cc51f752286b0b97e279f5cbf87
Mike Puangmalai <mike@nongaap.com>
2016-08-18 22:55:05 -0700

    event emitter not complete

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -20,8 +20,34 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
+ /*
+  Approach: EventEmitter is an object with 'on' and 'trigger' methods.
+  'on' accepts an 'event' string as the first argument and a function as a second argument that will be invoke if that 'event is triggered.
+  'on' method can be called multiple times to add multiple events and also to add multiple functions to be invoked to a single 'event'.
+  'trigger' will accept an 'event' string as the first argument and pass additional arguments to the listener 'on' function(s).
+  Create an 'event' object that saves each 'on' event as a key and stores the function in an array.
+  The 'trigger' method will look up the 'event' in the event object and invoke all the functions in the array (passing in any additional arguments into those functions)
+ */
+
 function EventEmitter() {
+	this.events = {};
+}
+
+EventEmitter.prototype.on = function(){
 
 }
+
+EventEmitter.prototype.trigger = function(){
+
+}
+
+ // var instance = new EventEmitter();
+ // var counter = 0;
+ // instance.on('increment', function() {
+ //    counter++;
+ // }); // counter should be 0
+ 
+ // instance.trigger('increment'); // counter should be 1
+ // instance.trigger('increment'); // counter should be 2
 
 module.exports = EventEmitter;
