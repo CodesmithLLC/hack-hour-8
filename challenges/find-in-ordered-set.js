6bd60d7e2ee9f847307ac99b962815fe7e5e5f9d
Steven Harlan <SteveHarlan@Stevens-MacBook-Air.local>
2016-08-19 09:42:51 -0700

    find in ordered set at O(Log N)

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -8,11 +8,23 @@ findInOrderedSet(nums, 4);  -> true
 findInOrderedSet(nums, 2);  -> false
 
  */
-
+//Going for O(Log n);
 
 function findInOrderedSet(arr, target) {
-
+  var mid = Math.floor(arr.length/2);
+  if(target === arr[mid]) {
+    return true;
+  } else if (target < arr[mid]){
+    return findInOrderedSet(arr.slice(0, mid), target);
+  } else if (target > arr[mid]){
+    return findInOrderedSet(arr.slice(mid + 1, arr.length), target);
+  } else {
+    return false;
+  }
 }
 
 
 module.exports = findInOrderedSet;
+
+var nums = [1, 4, 6, 7, 9, 17, 45, 46]
+console.log(findInOrderedSet(nums, 1))
