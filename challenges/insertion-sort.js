f8c233b9fdbdefe9c1b0ed547381cfbc6587a26b
campallison <allisonhanna@gmail.com>
2016-08-20 09:34:08 -0700

    working insertion sort. so slow. so, so slow.

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,26 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
+// so slow!
 
 function insertionSort(array) {
+  const swap = (arr, a, b) => {
+    const swapArr = arr;
+    const temp = swapArr[a];
+    swapArr[a] = swapArr[b];
+    swapArr[b] = temp;
+  };
+  let i;
+  let j;
 
+  for (i = 0; i < array.length; i++) {
+    j = i;
+    while (j > 0 && array[j - 1] > array[j]) {
+      swap(array, j, --j);
+    }
+  }
+
+  return array;
 }
 
 module.exports = insertionSort;
