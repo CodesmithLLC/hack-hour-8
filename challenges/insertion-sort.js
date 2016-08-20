e73b87fd69fc4c958cc5ab16bc9892fa33bad20f
Yandri Sanchez <ysanchez3d@gmail.com>
2016-08-20 09:47:39 -0700

    insertionSort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,6 +6,26 @@
 
 function insertionSort(array) {
 
+    for (var i = 1; i < array.length; i ++) {
+        var current = array[i];
+        var prev = array[i - 1]
+        
+        if (current >= prev) {
+            continue;
+        }
+        
+        for (var j = i; j > 0; j--) {
+            start = array[j];
+            prev = array[j - 1];
+            
+            if (prev > start ) {
+                array[j] = prev;
+                array[j - 1] = start;
+            }
+        }
+    }
+    
+    return array;
 }
 
 module.exports = insertionSort;
