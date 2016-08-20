b458ff9c9dd25d96426beab46b5f5bb44a3ce4cf
Micky Shaked <micky.shaked@gmail.com>
2016-08-20 09:32:53 -0700

    insertion sort complete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,16 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+	if (array.length === 1) return array;
+	for(var i = 1; i < array.length; i++){
+		var temp = array[i];
+		for(var j = i-1; j>=0 && array[j] > temp; j--){
+			array[j+1] = array[j];
+		}
+		array[j+1] = temp;
+	}
+	return array;
 }
 
 module.exports = insertionSort;
