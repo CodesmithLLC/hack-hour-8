64d815174cf5eb8bfa565eb71d4d93392c404da2
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-23 09:27:20 -0700

    complete hack-hour

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -6,11 +6,19 @@
  * this is done via:
  * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
  *
- * do not use division, becuase zero might be in the array and you cannot divide by zero
+ * do not use division, because zero might be in the array and you cannot divide by zero
  */
 
 function getAllProducts(array) {
+  const res = [];
 
+  for (let i = 0; i < array.length; i++) {
+    const numbers = [...array];
+    numbers.splice(i, 1);
+    res.push(numbers.reduce((a, b) => a * b));
+  }
+
+  return res;
 }
 
 module.exports = getAllProducts;
