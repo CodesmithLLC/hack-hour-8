d554d62dd449731f98a3b1234183ba9dec2be4b4
campallison <allisonhanna@gmail.com>
2016-08-23 10:09:15 -0700

    working solution for get all products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -6,11 +6,29 @@
  * this is done via:
  * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
  *
- * do not use division, becuase zero might be in the array and you cannot divide by zero
+ * do not use division, because zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
+function rotateArr(array) {
+  const multiplyArray = (a) => {
+    let p = 1;
+    let j;
+    for (j = 0; j < a.length; j++) {
+      p *= a[j];
+    }
+    return p;
+  };
+  let arr = array.slice();
 
+  let i;
+  let resArr = [];
+  for (i = array.length; i > 0; i--) {
+  const last = arr.splice(arr.length - 1, 1);
+  resArr.push(multiplyArray(arr));
+  arr.unshift(last);
+  }
+
+  return resArr;
 }
 
 module.exports = getAllProducts;
