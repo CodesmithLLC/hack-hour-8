1a5334d10d7c3f4dcc2c1ab3750f510070e3afb1
davidvorona <davidavorona@gmail.com>
2016-08-23 16:58:40 -0700

    refactored hP and finished all-products

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,34 +24,28 @@
  *
  */
 
- function balancedParens(input){
-   if (input.length === 1) return false;
-   input = input.split("");
-   var output = [];
-   for (var j = 0; j < input.length; j++) {
-   	if (input[j] === '(' || input[j] === ')' || input[j] === '{' || input[j] === '}' || input[j] === '[' || input[j] === ']') output.push(input[j]);
-   }
-   var count1 = 0;
-   var count2 = 0;
-   var count3 = 0;
-   for (var i = 0; i < output.length; i++) {
-   	if (count1 === -1 || count2 === -1 || count3 === -1) return false;
-   	if (count1 > 0 && count2 === 0 || count3 === 0) return false;
-   	if (count2 > 0 && count1 === 0 || count3 === 0) return false;
-   	if (count3 > 0 && count1 === 0 || count2 === 0) return false;
-     if (output[i] === '(') count1++;
-     if (output[i] === '[') count2++;
-     if (output[i] === '{') count3++;
-     if (output[i] === ')') count1--;
-     if (output[i] === ']') count2--;
-     if (output[i] === '}') count3--;
-   }
-   if (count1 === 0 && count2 === 0 && count3 === 0) return true;
-   return false;
+function balancedParens(str) {
+ 	function getBracs(str) {
+ 		var res = [];
+ 		for (var i = 0; i < str.length; i++) {
+ 			if (str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === ')' || str[i] === ']' || str[i] === '}') res.push(str[i]);
+ 		}
+ 		return res;
+ 	}
+ 	var bracs = getBracs(str);
+ 	var stack = [];
+ 	for (var i in bracs) {
+ 		var brac = brac[i]
+ 		if (brac == '(' || brac == '[' || brac == '{') {
+ 			stack.push(brac)
+ 		} else {
+ 			if (!stack.length) return false;
+ 			if (!bracMatch(stack.pop(), brac)) return false;
+ 		}
+ 	}
+ 	return !stack.length;
  }
- 
-function balancedParens(input){
 
-}
+ // define bracMatch
 
 module.exports = balancedParens;
--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,35 +3,11 @@
  */
 
 function highestProduct(array) {
-  var combos = [];
-
-  permutation(array, []);
-
-  function permutation(begArr,arr) {
-  	  if (begArr.length === 0) combos.push(arr.join(',').split(','));
-  	  for (var i = 0, l = begArr.length; i < l; i++) {
-  	    let s = begArr.splice(i,1);
-  	    arr.push(s);
-  	    permutation(begArr,arr);
-  	    arr.pop();
-  	    begArr.splice(i,0,s);
-  	  }
-  	}
-
-  	var finalArr = combos.map(function(arrToNum){
-  	   return arrToNum.map(function(e){
-  	  	return parseInt(e);
-  	   });
-  	  });
-
-  	var product = 0;
-  	for (var i = 0; i < finalArr.length; i++) {
-  		var current = finalArr[i][0]*finalArr[i][1]*finalArr[i][2];
-  		if (current > product) {
-  			product = current
-  		}
-  	}
-  	return product;
+ 	if (array.length < 4) return array.reduce(function(a,b) { return a*b; });
+ 	var sorted = array.sort(function(a,b) { return a - b; });
+ 	if ((sorted[0] * sorted[1]) > (sorted[sorted.length - 2] * sorted[sorted.length - 3])) {
+ 		return sorted[0] * sorted[1] * sorted[sorted.length - 1];
+ 	} else return sort[sorted.length - 1] * sorted[sorted.length - 2] * sorted[sorted.length - 3];
 }
 
 
