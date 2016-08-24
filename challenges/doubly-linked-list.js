3db3053cd86170ea76aee46460377ec4ebeff3a8
Micky Shaked <micky.shaked@gmail.com>
2016-08-24 09:47:21 -0700

    cant get tail to update to reflect removed node

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,38 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+	let node = new Node(val);
+	if(!this.head){
+		this.head = node;
+		this.tail = node;
+	} else {
+		this.tail.next = node;
+		node.prev = this.tail;
+		this.tail = node;
+	}
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+	var node = this.head
+ 	if(val === this.head.val) {
+  		this.head = node.next;
+  		return node.val;
+	}
+	else{
+		while(val !== node.val){
+			node = node.next;
+		}
+		if(!node.next){
+			node = this.tail;
+			this.tail = node.prev;
+			this.tail.next = null;
+		}
+		node.prev.next = node.next;
+		return node.val;
+	}
 };
 
 module.exports = LinkedList;
