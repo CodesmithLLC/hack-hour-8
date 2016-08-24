69636c02723fd14892aa569dde20652ec7f9aba0
campallison <allisonhanna@gmail.com>
2016-08-24 11:49:54 -0700

    doubly linked list implementation

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -1,6 +1,4 @@
-/*
-Create a doubly linked list with an add and remove method
- */
+/* Create a doubly linked list with an add and remove method */
 
 function LinkedList() {
   this.head = null;
@@ -13,18 +11,36 @@ function Node(val) {
   this.prev = null;
 }
 
-/*
-Adds a node to the end of the list
- */
-LinkedList.prototype.add = function(val) {
-  
+/* Adds a node to the end of the list */
+
+LinkedList.prototype.add = (val) => {
+  if (typeof val === 'undefined') return false;
+
+  const node = new Node(val);
+
+  if (!this.head) {
+    this.head = this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = this.tail.next;
+  }
+
+  this.tail.val = val;
 };
 
-/*
-Removes the first node with the inputted value
- */
-LinkedList.prototype.remove = function(val) {
-  
+/* Removes the first node with the inputted value */
+LinkedList.prototype.remove = (val) => {
+  let curr = this.head;
+
+  while (curr) {
+    if (curr.val === val) {
+      curr.prev = curr.prev.prev;
+      curr.next = curr.next.next;
+    }
+
+    curr = curr.next;
+  }
 };
 
 module.exports = LinkedList;
