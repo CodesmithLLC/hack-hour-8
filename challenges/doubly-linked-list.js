8ac00bf438965b7176afc1d22535d7712e798aa6
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-24 09:44:41 -0700

    complete doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,34 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (this.head) {
+    this.tail.next = new Node(val);
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
+  } else {
+    this.head = this.tail = new Node(val);
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (this.head.val === val) {
+    this.head = this.head.next;
+    this.head.prev = null;
+    return;
+  }
+
+  let node = this.head.next;
+  while (node) {
+    if (node.val === val) {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+      return node;
+    }
+    node = node.next;
+  }
 };
 
 module.exports = LinkedList;
