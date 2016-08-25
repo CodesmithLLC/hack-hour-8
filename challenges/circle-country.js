8d0b0d13b09b415d5439ed08c724e2ae0d554e33
campallison <allisonhanna@gmail.com>
2016-08-25 10:51:40 -0700

    stashing progress

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -1,18 +1,22 @@
-/**
+/*
  *  Circle Country is a country that contains several circular-shaped districts.
- *  Some districts may be situated inside other districts, but their borders do not intersect or touch.
- *  Tyus is a resident of Circle Country. When he travels between two locations, he always tries to cross the fewest number of district borders as possible.
+ *  Some districts may be situated inside other districts, but their borders do
+ * not intersect or touch.
+ *  Tyus is a resident of Circle Country. When he travels between two
+ * locations, he always tries to cross the fewest number of district borders as possible.
  *
  *  You are given an array x, an array y, and an array r
  *
  *    - (x[i],y[i]) are the coordinates of the i-th circle's center and r[i] is its radius
  *
- *  In addition to the arrays, you are also given the numbers start_x, start_y, end_x, and end_y
+ *  In addition to the arrays, you are also given the numbers startX, startY, end_x, and end_y
  *
- *    - (start_x, start_y) are Tyus' starting coordinates and (end_x, end_y) are Tyus' destination coordinates
+ *    - (start_x, start_y) are Tyus' starting coordinates and (end_x, end_y)
+ * are Tyus' destination coordinates
  *
  *
- *  Write an efficient algorithm for finding the minimum number of district borders Tyus must cross in order to get from
+ *  Write an efficient algorithm for finding the minimum number of district
+ * borders Tyus must cross in order to get from
  *  (start_x, start_y) to (end_x, end_y)
  *
  *  Constraints:
@@ -22,8 +26,16 @@
  *
  */
 
-function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
+function circleCountry(x, y, r, startX, startY, endX, endY) {
+  const distanceFromStartToEnd = Math.sqrt(Math.pow(Math.abs(startX - endX), 2) + Math.pow(Math.abs(startY - endY), 2));
+  let i;
+  let minBorders;
 
+  for (i = 0; i < x.length; i++) {
+
+  }
+
+  return minBorders;
 }
 
 module.exports = circleCountry;
