84bea68e949a0ecb6f136fbc9d3a3f2473052e8d
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-26 09:34:17 -0700

    complete bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  for (var i = 0; i < array.length - 1; i++) {
+    let j = i;
+    while (array[j] > array[j + 1]) {
+      let temp = array[j + 1];
+      array[j + 1] = array[j];
+      array[j] = temp;
+      j++;
+      i = -1;
+    }
+  }
 }
 
 module.exports = bubbleSort;
