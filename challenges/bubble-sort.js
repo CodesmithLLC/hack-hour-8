8ebd54e1cf7ed60792f88dfc181a6ac5d0704b6a
Shray bansal <bansalshray@gmail.com>
2016-08-26 09:50:12 -0700

    finished bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
-
+	var temp;
+	var unsorted = true;
+	while(unsorted) {
+		unsorted = false;
+		for (var i = 0; i < array.length-1; i++) {
+			if(array[i] > array[i+1]) {
+				temp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = temp;
+				unsorted = true;
+			}
+		}
+	}
+	return array;
 }
 
 module.exports = bubbleSort;
