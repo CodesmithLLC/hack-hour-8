de8b78b684e375a7ada45f9abb50db3db15eddca
campallison <allisonhanna@gmail.com>
2016-08-26 09:27:36 -0700

    bubble sort solution

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,27 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
+  const swap = (arr, a, b) => {
+    const array = arr;
+    const temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+  }
+  let end = array.length - 2;
+  let madeSwap;
+  let i;
 
+  do {
+    madeSwap = false;
+    for (i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        madeSwap = true;
+      }
+    }
+  } while (end-- && madeSwap);
+  return array;
 }
 
 module.exports = bubbleSort;
