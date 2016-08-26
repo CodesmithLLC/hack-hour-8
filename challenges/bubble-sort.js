f38d3801eb50e8d8b45296f882bc42a094da947c
Mike Puangmalai <mike@nongaap.com>
2016-08-26 09:47:47 -0700

    bubble sort complete

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,17 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
-
+  for(var i = 0; i < array.length; i++){
+    for(var j = array.length-1; j > 0; j--){
+  	  if(array[array.length-j] < array[array.length-j-1]){
+  	  	var temp1 = array[array.length-j];
+  	  	var temp2 = array[array.length-j-1];
+  	  	array.splice(array.length-j-1,2,temp1,temp2);
+  	  } 
+  	}
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
