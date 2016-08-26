cd7dc49436dec79d3f6980dcea42a5e14e9c5f9f
Steven Harlan <SteveHarlan@Stevens-MacBook-Air.local>
2016-08-26 09:19:13 -0700

    08262016

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,16 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      const temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
+      return bubbleSort(array);
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
