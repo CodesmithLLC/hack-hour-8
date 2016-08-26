fef30c9fc34a1fea8987385c0111569aef172678
Cole Williams <cowi4030@colorado.edu>
2016-08-26 09:24:56 -0700

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,8 +4,20 @@
 
 
 
+
+
+
 function bubbleSort(array) {
-
+ var temp;
+  for (var i = 0; i < array.length; i++) { 
+    for (var j = 0; j < (array.length - i - 1); j++) { 
+      if(array[j] > array[j+1]) {
+        
+        temp = array[j];  
+        array[j] = array[j+1]; 
+        array[j+1] = temp; 
+      }
+    }        
+  }
 }
-
 module.exports = bubbleSort;
