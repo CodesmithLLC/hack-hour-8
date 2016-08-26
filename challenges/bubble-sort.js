fcb4bfb261703e15eb48fefd52fe9eed2e69d955
= <ysanchez3d@gmail.com>
2016-08-26 09:51:24 -0700

    bubbleSort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -6,6 +6,25 @@
 
 function bubbleSort(array) {
 
+	while (true) {
+		let counter = 0;
+
+		for (let i = 1; i < array.length; i++) {
+
+
+			let first = array[i - 1];
+			let second = array[i];
+
+			if (second < first) {
+				array[i - 1] = second;
+				array[i] = first;
+				counter++;
+			}
+		}
+		
+		if (counter === 0)  return array;
+	}
+
 }
 
 module.exports = bubbleSort;
