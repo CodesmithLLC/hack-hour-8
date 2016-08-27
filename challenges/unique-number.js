9f5fdcf5f857496ba65e6f51dc077fea77ccf6bc
zhiwenhuang <zhiwen555@gmail.com>
2016-08-27 10:00:13 -0700

    phonebook

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,24 +9,34 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
-  var cache = {};
-  for (var i = 0; i < array.length; i++) {
-    if (!cache.hasOwnProperty(array[i])) {
-      cache[array[i]] = 0;
-    } else {
-      cache[array[i]] = cache[array[i]] + 1;
-    }
-  }
+// function uniqueNumber(array) {
+//   var cache = {};
+//   for (var i = 0; i < array.length; i++) {
+//     if (!cache.hasOwnProperty(array[i])) {
+//       cache[array[i]] = 0;
+//     } else {
+//       cache[array[i]] = cache[array[i]] + 1;
+//     }
+//   }
+//
+//   for (var key in cache) {
+//     if (cache[key] === 0) {
+//       return key;
+//     }
+//   }
+//   console.log(cache);
+// }
 
-  for (var key in cache) {
-    if (cache[key] === 0) {
-      return key;
-    }
+function uniqueNumber(array) {
+  var res = 0;
+  for (var i = 0; i < array.length; i++) {
+    // console.log('operation ' + res + ' ^ ' + array[i]);
+    res = res ^ array[i];
+    // console.log((res >>> 0).toString(2));
   }
-  console.log(cache);
+  return res;
 }
 
-// console.log(uniqueNumber([1,2,1,3,3]));
+console.log(uniqueNumber([1,2,1,3,3]));
 
 module.exports = uniqueNumber;
