c18ae0f4dfaf456df978c42ce3643c61f01b08e3
Blake Watkins <iamsoblake@gmail.com>
2016-08-27 13:57:51 -0700

    phonebooked

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -1,30 +1,35 @@
+'use strict';
 /**
-* You are given an array of arrays.  The subarrays are first names and phone numbers.
-*
-* Example:
-*   jazbook = [
-*     ['alex','301-844-3421'],
-*     ['jae','301-844-1211'],
-*     ['david','301-844-0978'],
-*     ['travis','301-844-8505']
-*     ['jasmine','1800-974-4539'],
-*   ];
-*
-* jazbooks are not always sorted...
-*
-* Develop a function that takes in a jazbook and a name, searches through the jazbook and 
-* returns the persons phone number. If the person does not exists, return false.
-*
-* How efficient can you make this?
-*/
+ * You are given an array of arrays.  The subarrays are first names and phone numbers.
+ *
+ * Example:
+ *   jazbook = [
+ *     ['alex','301-844-3421'],
+ *     ['jae','301-844-1211'],
+ *     ['david','301-844-0978'],
+ *     ['travis','301-844-8505']
+ *     ['jasmine','1800-974-4539'],
+ *   ];
+ *
+ * jazbooks are not always sorted...
+ *
+ * Develop a function that takes in a jazbook and a name, searches through the jazbook and 
+ * returns the persons phone number. If the person does not exists, return false.
+ *
+ * How efficient can you make this?
+ */
 
 function findName(jazbook, name) {
-  
-  return number;
+
+  return makeJazBookIntoARealPhoneBookObject(jazbook)[name] ? makeJazBookIntoARealPhoneBookObject(jazbook)[name] : false;
 }
 
-function makeJazBookIntoARealPhoneBookObject(jazbook){
+function makeJazBookIntoARealPhoneBookObject(jazbook) {
 
+  var phonebook = {};
+  jazbook.forEach((ele, idx) => {
+    phonebook[ele[0]] = ele[1];
+  })
   return phonebook;
 }
 
@@ -34,4 +39,4 @@ var objectToExport = {
   makeJazBookIntoARealPhoneBookObject: makeJazBookIntoARealPhoneBookObject
 };
 
-module.exports=objectToExport;
+module.exports = objectToExport;
