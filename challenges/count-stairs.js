e9636e5ec1658522279b689d95374a0b6502b988
binh.x.nguyen <binhxn89@gmail.com>
2016-08-29 10:09:24 -0700

    count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -3,7 +3,22 @@
  */
 
 function countStairs(n) {
+	// have counter to keep track of how many permutations were made
+	let counter = 0;
 
+	// base case to end recursion
+	if (n === 0) return 0;
+	if (n === 1) return 1;
+
+	// after first step, it should enter recursion where it will take 1 step or 2 steps,
+	while (n > 0) {
+
+		// subtract the amount of steps from n and repeat until n = 0.
+		return countStairs(n - 1) + countStairs(n - 2);
+		counter++;
+	}
+
+	return counter;
 }
 
 module.exports = countStairs;
