43e52aaa0070cc66f983edb3afc144d05d7eaa5f
Blake Watkins <iamsoblake@gmail.com>
2016-08-29 09:48:29 -0700

    counted the stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,8 +2,20 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
-function countStairs(n) {
+//there's a pattern here, where n represents the nth + 1 fibonacci number.
+//So we'll use the fib function with this new knowledge and apply it in this
+//function to get the amount of possibilities.
 
+function countStairs(n) {
+  return fib(n + 1);
+}
+
+function fib(n){
+  if(n === 0) return 0;
+  if(n === 1) return 1;
+
+  return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = countStairs;
+
