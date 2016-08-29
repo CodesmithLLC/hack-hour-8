92f9a3b0b97cfa29b5674add0cca9e4b25f856fe
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-08-29 10:11:10 -0700

    count stairs test

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,8 +2,29 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
-function countStairs(n) {
 
+function countStairs(n) {
+    /*Approach: recursion.  All permutations to get to a certain number.  
+    */
+    if (n === 0) return 0; 
+    if (n === 1) return 1; 
+    let totalWays = 0, sum = 0; 
+    function addOneorTwo (sum){
+     if (sum === n) {
+        console.log('sum match', sum)
+        totalWays++;
+        }
+     else if (sum > n) return null; 
+     else {
+        console.log('sum', sum); 
+        return addOneorTwo(sum += 2) + addOneorTwo(sum += 1);
+     }
+    }
+    addOneorTwo(0); 
+    return totalWays; 
+    
+    
 }
+countStairs(5)
 
 module.exports = countStairs;
