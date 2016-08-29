b4e57c53497e046a1a4a86caa6961d322906275b
Mike Puangmalai <mike@nongaap.com>
2016-08-29 10:05:45 -0700

    initial pr

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,7 +2,17 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
+ /*
+You can choose from n single steps + 0 double steps to Math.floor n/2 double steps + (n - Math.floor n/2) single steps
+ */
+
 function countStairs(n) {
+  if(n <= 4) {
+    return n;
+  }
+  var steps = [1,2];
+  var count;
+
 
 }
 
