568dae71cd812392eaafa33ba9249fbf003d24a9
= <ysanchez3d@gmail.com>
2016-08-30 09:13:46 -0700

    update

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,8 +2,45 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
-function countStairs(n) {
+ function countStairs(n) {
+ 	var ans = 0;
+ 	var totalStairs = n;
 
-}
+ 	if (n === 0) return 0;
+ 	if (n === 1) return 1;
+
+ 	let steps = [];
+ 	while (n > 0) {
+ 		steps.push(1);
+ 		n--;
+ 		ans++;
+ 	};
+
+
+ 	var total = 0;
+ 	var sliced = false;
+ 	for (var i = 0; i < steps.length; i++) {
+ 		steps[i] === 2;
+ 		for (var j = 0; j < steps.length; j++) {
+
+ 			if (total + steps[j] <= totalStairs) {
+ 				total += steps[i];
+
+ 				if (sliced) {
+ 					ans++;
+ 				}
+ 			} else {
+ 				steps.splice(0, j);
+ 				sliced = true;
+ 				ans++;
+ 				total = 0;
+ 				break;
+ 			}
+  }
+
+ 	}
+
+ 	return ans;
+ }
 
 module.exports = countStairs;
