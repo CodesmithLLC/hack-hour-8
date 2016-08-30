500db500ddc645e1d21582789921ba5f264e095a
davidvorona <davidavorona@gmail.com>
2016-08-30 13:10:33 -0700

    count-stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -3,7 +3,19 @@
  */
 
 function countStairs(n) {
-
+  var combos = [];
+  var sum = 0;
+  // function combinations(target) {
+  //   if (sum === n) return combos.push(target);
+  //   if (sum === n-1) return combos.push(target.concat([1]))
+  //   for (var i = 1; i < 3; i++) {
+  //     target.push(i);
+  //     sum = sum + i;
+  //     combinations(target);
+  //   }
+  // }
+  // combinations([]);
+  return combos;
 }
 
 module.exports = countStairs;
