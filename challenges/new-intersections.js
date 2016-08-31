1d8fe52d7a43efd6339917cb7c1f804fb96b3641
Blake Watkins <iamsoblake@gmail.com>
2016-08-30 22:29:38 -0700

    needs refactoring and still not fully clear on output formatting

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  *
  * You are given an array x and an array y that represent the coordinates of several OLD points
@@ -16,8 +17,54 @@
  * 	 
  */
 
-function newIntersections(x, y){
+function newIntersections(x, y) {
+  if (x.length < 4 || y.length < 4) return [];
 
+  let xCache = makeCache(x),
+    // yCache = makeCache(y),
+    xKeys = Object.keys(xCache),
+    // yKeys = Object.keys(yCache),
+    xIntersects = [],
+    yIntersects = [];
+
+  for(let k in xCache){
+    if(xCache[k].length > 1){
+      var pairs = pairPermutes(xCache[k]);
+      pairs.forEach( (ele, idx) => {
+        y.forEach( (el, id) => {
+          if(el > Math.min(y[ele[0]], y[ele[1]]) && el < Math.max(y[ele[0]], y[ele[1]])){
+            xIntersects.push(k)
+            yIntersects.push(el)
+          }
+        })        
+      })
+    }
+  }
+
+  return [xIntersects, yIntersects];
+}
+
+function makeCache(array) {
+  let cache = {};
+  array.forEach((ele, idx) => {
+    if (cache[ele]) {
+      cache[ele].push(idx)
+    } else {
+      cache[ele] = [idx];
+    }
+  })
+  return cache;
+}
+
+function pairPermutes(array){
+  var result = [],
+    len = array.length;
+  array.forEach( (ele, idx) => {
+    for(var i = idx + 1; i < len - 1; i++){
+      result.push([ele, array[i]]);
+    }
+  })
+  return result;
 }
 
 module.exports = newIntersections;
