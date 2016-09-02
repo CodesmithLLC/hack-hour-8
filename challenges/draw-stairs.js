8d8d96595d7494c4f685e3fb775f16b0dab4209a
campallison <allisonhanna@gmail.com>
2016-09-02 10:49:29 -0700

    found a commented out merge conflict. corrected it

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,7 +1,6 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
-<<<<<<< HEAD
 
  For example:
  drawStairs(6) ->
