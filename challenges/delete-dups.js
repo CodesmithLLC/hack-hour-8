c7d2f65fd7be628eef5ef12d145cbb0d3fa5007e
davidvorona <davidavorona@gmail.com>
2016-09-03 16:00:18 -0700

    fixing hr

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,21 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
-
 function deleteDups(head) {
-
+  var current = head;
+  var previous;
+  while (current && current.next) {
+    previous = current;
+    while (previous.next) {
+      if (current.value === previous.next.value) {
+        previous.next = previous.next.next;
+      }
+      else {
+        previous = previous.next;
+      }
+    }
+    current = current.next;
+  }
 }
 
 module.exports = deleteDups;
