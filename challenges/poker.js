ab82390d029d61127f0a34c857db5c17ebe2f84e
Chandra Lindy <chandra.lindy@gmail.com>
2016-09-03 10:03:37 -0700

    partial attempts for poker and observed pin

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,7 +19,114 @@
 */
 
 function poker(hand1, hand2) {
+  function isfourOfAKind(hand) {
+    const cache = {};
+    const result = [];
 
+    hand.forEach(el => {
+      if (cache[el]) cache[el] += 1;
+      else cache[el] = 1;
+    });
+
+    const cards = Object.keys(cache);
+
+    if (cards.length === 2) {
+      if (cache[cards[0]] === 4 || cache[cards[1]] === 4) return true
+    }
+
+    return false;
+  }
+
+  function isFullHouse(hand) {
+    const cache = {};
+    const result = [];
+
+    hand.forEach(el => {
+      if (cache[el]) cache[el] += 1;
+      else cache[el] = 1;
+    });
+
+    const cards = Object.keys(cache);
+
+    if (cards.length === 2) {
+      if (cache[cards[0]] === 3 && cache[cards[1]] === 2) return true;
+      if (cache[cards[1]] === 3 && cache[cards[0]] === 2) return true;
+    }
+
+    return false;
+  }
+
+  function isStraight(hand) {
+    const cards = [...hand];
+    cards.sort((a, b) => a - b);
+
+    for (let i = 0; i < cards.length - 1; i++) {
+      if (Math.abs(cards[i] - cards[i + 1]) !== 1) return false;
+    }
+
+    return true;
+  }
+
+  function isThreeOfAKind(hand) {
+    const cache = {};
+    const result = [];
+
+    hand.forEach(el => {
+      if (cache[el]) cache[el] += 1;
+      else cache[el] = 1;
+    });
+
+    const cards = Object.keys(cache);
+
+    if (cards.length === 3) {
+      if (cache[cards[0]] === 3 ||
+          cache[cards[1]] === 3 ||
+          cache[cards[2]] === 3
+          ) return true;
+    }
+
+    return false;
+  }
+
+  function isPair(hand) {
+    const cache = {};
+    const result = [];
+
+    hand.forEach(el => {
+      if (cache[el]) cache[el] += 1;
+      else cache[el] = 1;
+    });
+
+    const cards = Object.keys(cache);
+
+    if (cards.length === 4) {
+      if (cache[cards[0]] === 2 ||
+          cache[cards[1]] === 2 ||
+          cache[cards[2]] === 2 ||
+          cache[cards[3]] === 2
+          ) return true;
+    }
+
+    return false;
+  }
+
+  function highCard(hand) {
+    const cache = {};
+    const result = [];
+
+    hand.forEach(el => {
+      if (cache[el]) cache[el] += 1;
+      else cache[el] = 1;
+    });
+
+    const cards = Object.keys(cache);
+
+    if (cards.length === 5) {
+      
+    }
+
+    return false;
+  }
 }
 
 module.exports = poker;
