db37caad7adc73ad7d19ee1968e06edfee24b468
Mike Puangmalai <mike@nongaap.com>
2016-09-06 09:11:07 -0700

    poker work in progress

--- challenges/poker.js
+++ challenges/poker.js
@@ -1,7 +1,7 @@
 /*Build a function that takes in two arrays of five elements, with each
 * array representing a player's hand and each element
 * representing a card from a standard deck of cards:
-* 52 cards, 2 - Ace, NO suits. (Jack = 11, Queen = 12, King = 13, Ace = 14)
+* 52 cards, 2 through Ace, NO suits. (Jack = 11, Queen = 12, King = 13, Ace = 14)
 *
 * The function should return either the string "Player 1 wins", "Player 2 wins", or "Draw"
 *  based on the two player's hands:
@@ -18,8 +18,65 @@
 * Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
 */
 
+
+function card(){
+  return Math.floor(Math.random()*(13)+2);; 
+}
+
+//returns an array of poker hands (array or arrays) for 52 card deck. max 10 players.
+function pokerHand(players){
+  if(players > 10) {
+  	return new Error('Too Many Players! 10 max players');
+  }
+  var hands = [];
+  var deck = {
+    2: 4,
+    3: 4,
+    4: 4,
+    5: 4,
+    6: 4,
+    7: 4,
+    8: 4,
+    9: 4,
+    10: 4,
+    11: 4,
+    12: 4,
+    13: 4,
+    14: 4
+  };
+  // for(var i = 0; i < players; i++){
+  //   for(var j = 0; j < 5; j++){
+  //     var pick = card();
+  //     if(deck[pick] > 0){
+      	
+  //     }
+  //   }
+  // }
+  return hands;
+}
+
 function poker(hand1, hand2) {
 
 }
+
+// function swiz(num){
+// switch (num) {
+//   case 1:
+//     console.log("Player 1 wins");
+//     break;
+//   case 2:
+//     console.log("Player 2 wins");
+//     break;
+//     break;
+//   default:
+//     console.log("Draw");
+// }
+// }
+
+// swiz(1);
+// swiz(2);
+// swiz(3);
+
+//console.log(pokerHand(11));
 
 module.exports = poker;
