a48fab33fe2fc13de2eefd4a3896856368043adf
zhiwenhuang <zhiwen555@gmail.com>
2016-09-09 09:16:32 -0700

    push

--- challenges/missing-number.js
+++ challenges/missing-number.js
@@ -1,6 +1,6 @@
-/* 
-A zero-indexed array A consisting of N different integers is given. 
-The array contains integers in the range [1..(N + 1)], which means 
+/*
+A zero-indexed array A consisting of N different integers is given.
+The array contains integers in the range [1..(N + 1)], which means
 that exactly one element is missing.Your goal is to find that missing element.
 Write a function that, given an array A, returns the value of the missing element.
 
@@ -10,7 +10,7 @@ For example, given array A such that:
   A[1] = 3
   A[2] = 1
   A[3] = 5
-the function should return 4, as it is the missing element.
+the function should return 4, as it is the missing element. ccc
 
 Assume that:
  * N is an integer within the range [0..100,000];
@@ -21,7 +21,7 @@ Assume that:
 expected worst-case time complexity is O(N);
 expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 
-Challange: 
+Challange:
   ** cannot use additional storage, variables are okay not any TYPE of object
   ** keep in mind time complexity
 */
