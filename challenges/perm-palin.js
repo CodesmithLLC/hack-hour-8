17ab60be80e831cdc41aafc4b39e78d65904fae0
campallison <allisonhanna@gmail.com>
2016-09-12 09:39:30 -0700

    stashing progress

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,28 @@
  */
 
 function permPalin(str) {
-	
+  const letters = str.split('');
+  const letterObj = {};
+  const freq = [];
+  let i;
+  let item;
+
+  for (i = 0; i < letters.length; i++) {
+    if (!letterObj[letters[i]]) {
+      letterObj[letters[i]] = 1;
+    } else if (letterObj[letters[i]]) {
+      letterObj[letters[i]] += 1;
+    }
+  }
+
+  console.log(letterObj);
+  console.log(Object.keys(letterObj));
+
+  for (item in letterObj) {
+    freq.push(letterObj[item]);
+  }
+
+  return letterObj;
 }
 
 module.exports = permPalin;
