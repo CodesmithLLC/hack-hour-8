6937a43aae1af52036408ff4a25ffe2de8182f34
Mike Puangmalai <mike@nongaap.com>
2016-09-12 09:09:54 -0700

    each permutation not finished

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -16,10 +16,41 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
+// function eachPermutation(arr, callback) {
+//   for(var i = 0; i < arr.length; i++){
+  	
+//   }
 
+// }
+
+function eachPermutation(arraysToCombine) {
+    var divisors = [];
+    for (var i = arraysToCombine.length - 1; i >= 0; i--) {
+       divisors[i] = divisors[i + 1] ? divisors[i + 1] * arraysToCombine[i + 1].length : 1;
+    }
+
+    function getPermutation(n, arraysToCombine) {
+       var result = [], 
+           curArray;    
+       for (var i = 0; i < arraysToCombine.length; i++) {
+          curArray = arraysToCombine[i];
+          result.push(curArray[Math.floor(n / divisors[i]) % curArray.length]);
+       }    
+       return result;
+    }
+
+    var numPerms = arraysToCombine[0].length;
+    for(var i = 1; i < arraysToCombine.length; i++) {
+        numPerms *= arraysToCombine[i].length;
+    }
+
+    var combinations = [];
+    for(var i = 0; i < numPerms; i++) {
+        combinations.push(getPermutation(i, arraysToCombine));
+    }
+    return combinations;
 }
 
-
+console.log(eachPermutation([1, 2, 3]));
 
 module.exports = eachPermutation;
