5aa1b756ba98428df0b00ea30a9b9f1d600aeee8
Mike Puangmalai <mike@nongaap.com>
2016-09-12 09:47:33 -0700

    perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,33 @@
  */
 
 function permPalin(str) {
-	
+  var data = str.split('');
+  var permutations = [],
+  stack = [],
+  output = false; //default output 
+
+  function doPerm() {
+    if (data.length == 0) {
+      permutations.push(stack.slice());
+    }
+    for (var i = 0; i < data.length; i++) {
+      var character = data.splice(i, 1);
+      stack.push(character);
+      doPerm();
+      stack.pop();
+      data.splice(i, 0, character);
+    }
+  }
+
+  doPerm();
+  for(var i = 0; i < permutations.length; i++){
+  	if(permutations[i].join('') === permutations[i].reverse().join('')) {
+  		return output = true;
+  	}
+  }
+
+  return output;
+
 }
 
 module.exports = permPalin;
