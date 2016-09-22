78e097e03df773807010831b9dd1ca56fceb826a
Blake Watkins <iamsoblake@gmail.com>
2016-09-22 15:26:47 -0700

    finished linked list cycle but not in constant space

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * We are familar with linked lists being linear and terminating:
  *
@@ -34,6 +35,28 @@ var Node = function(value) {
 
 function hasCycle(head) {
 
+  let nodeArr = [];
+  let curr = head;
+
+  while (curr) {
+    nodeArr.push(curr);
+    if (nodeArr.indexOf(curr.next) > -1) {
+      return true;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return false;
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => false
+// hasCycle(node1); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
