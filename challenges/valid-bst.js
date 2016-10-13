79dd7d67daa22d0c91f994b856848c68b3cd74ae
Blake Watkins <iamsoblake@gmail.com>
2016-10-13 09:58:32 -0700

    Fixing bad export issues

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -1,4 +1,3 @@
-'use strict';
 /* You are given a tree. Write a function to check if it is a valid binary search tree. A tree is
  * a valid binary search tree if it satisfies the following constraints:
  *      at any given node, the value of all the nodes in its left tree must be <= its value
