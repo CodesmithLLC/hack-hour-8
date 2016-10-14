2d9bc51417ec32df4dfe48220446dbe35677ec61
Blake Watkins <iamsoblake@gmail.com>
2016-10-14 16:03:45 -0700

    got add linked list working and tested

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -1,4 +1,4 @@
-//This definitely needs to be redone
+// This definitely needs to be redone
 
 /* You have two numbers represented by linked lists. Each node contains a single digit. The digits
  * are stored in reverse order, such that the 1's digit is at the head of the list. Write
@@ -15,55 +15,38 @@ function Node(val) {
   this.next = null;
 }
 
-function flattenlist(list){
-  var current = list;
-  var numstr = '';
-  while(typeof current.value === 'number'){
-    numstr = current.value + numstr;
-    current = current.next;
+function addLinkedList(l1, l2) {
+  let firstCurr = l1,
+    secondCurr = l2,
+    nodeCurr,
+    carryOver = 0,
+    sum = null,
+    node = null;
+
+  while (firstCurr || secondCurr || carryOver) {
+    if (!firstCurr && !secondCurr && carryOver) {
+      sum = carryOver;
+      carryOver = 0;
+      nodeCurr.next = new Node(sum);
+      return node;
+    } else if (!firstCurr || !secondCurr) {
+      sum = !firstCurr ? secondCurr.value + carryOver : firstCurr.value + carryOver;
+      carryOver = 0;
+    } else {
+      sum = firstCurr.value + secondCurr.value + carryOver;
+      sum > 9 ? (carryOver = +(sum / 10).toFixed(), sum %= 10) : carryOver = 0;
+    }
+
+    !node ? (node = new Node(sum), nodeCurr = node) : (nodeCurr.next = new Node(sum), nodeCurr = nodeCurr.next)
+
+    firstCurr = !firstCurr ? null : firstCurr.next;
+    secondCurr = !secondCurr ? null : secondCurr.next;
   }
-  return +numstr;
+
+  return node;
 }
 
-function singlist() {
-    this._length = 0;
-    this.head = null;
-}
-
-singlist.prototype.add = function(value) {
-    var node = new node(value),
-        currentnode = this.head;
- 
-    // 1st use-case: an empty list 
-    if (!currentnode) {
-        this.head = node;
-        this._length++;
-         
-        return node;
-    }
- 
-    // 2nd use-case: a non-empty list
-    while (currentnode.next) {
-        currentnode = currentnode.next;
-    }
- 
-    currentnode.next = node;
- 
-    this._length++;
-     
-    return node;
+module.exports = {
+  Node: Node,
+  addLinkedList: addLinkedList
 };
-
-function addlinkedlist(l1, l2) {
-
-  var sum = flattenlist(l1) + flattenlist(l2);
-  var revarr = sum.tostring.split('').reverse();
-  var list = new singlist();
-  for(var i = 0; i < revarr; i++){
-    list.add(+revarr[i]);
-  }
-
-  return list;
-}
-
-module.exports = {Node: Node, addLinkedList: addLinkedList};
