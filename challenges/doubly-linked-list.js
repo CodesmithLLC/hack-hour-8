ca9182e78a864eabb0a48c95c8525f69088e81d4
Blake Watkins <iamsoblake@gmail.com>
2016-10-16 10:33:09 -0700

    Doubly linked list tested and done

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,13 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  if (!this.tail) {
-    this.tail = new Node(val);
-    this.tail.prev = this.head;
+  if (!this.head) {
+    this.tail = this.head = new Node(val);
+  } else {
+    const prev = this.tail;
+    this.tail = this.tail.next = new Node(val);
+    this.tail.prev = prev;
   }
-
-  let temp = this.tail;
-  this.tail = new Node(val);
-  this.tail.prev = temp;
 };
 
 /*
@@ -38,18 +37,19 @@ LinkedList.prototype.remove = function(val) {
     if (curr.val === val && curr === this.head) {
       this.head = curr.next;
       curr = this.head;
-      curr.prev = null;
+      return curr.prev = null;
     }
 
     if (curr.val === val) {
       temp = curr.next;
       curr.next.prev = curr.prev;
-      curr.prev.next = temp;
+      return curr.prev.next = temp;
     }
 
     if (curr.next === this.tail && curr.next.val === val) {
-      this.tail = curr;
       curr.next = null;
+      this.tail = curr;
+      return this.tail.prev = curr.prev
     }
     curr = curr.next;
   }
