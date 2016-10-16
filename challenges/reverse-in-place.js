f4891c4359fdb2ee05f9422bc9660832d0cbfdf7
Blake Watkins <iamsoblake@gmail.com>
2016-10-16 14:38:02 -0700

    reversed in placed tested and finished

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -10,21 +10,22 @@
  * Since strings are immutable in javascript, we will be reversing an array of characters instead.
  *
  */
-var thing = ['a', 'b','c','d'];
+let thing = ['a', 'b', 'c', 'd'];
 
 function reverseInPlace(array) {
-  //No 'new objects' created, but still things created..
-  var temp;
-  var len = array.length - 1;
-  for(var i = 0; i < array.length / 2;i++){
+  // No 'new objects' created, but still things created..
+  if (!array || !array.length) return [];
+  let temp;
+  let len = array.length - 1;
+  for (let i = 0; i < array.length / 2; i++) {
     temp = array[i];
     array[i] = array[len];
     array[len] = temp;
     len--;
   }
-  return array; 
+  return array;
 
-  //Easy solution
+  // Easy solution
   // return array.reverse();
 }
 
