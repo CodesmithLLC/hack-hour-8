8ed2505fd94dccad6fdfd28981e91146bcfdb0e4
Blake Watkins <iamsoblake@gmail.com>
2016-10-16 12:13:34 -0700

    finished tested and simplified insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,28 +2,23 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-var test = [3,7,4,9,5,2,6,1];
+// var test = [3, 7, 4, 9, 5, 2, 6, 1];
+// var test2 = [9, 7, 8, 7, 8, 0, 2];
 
 function insertionSort(array) {
-  var copyArr = array.slice(),
-    temp;
-  
-  copyArr.forEach(function(ele,idx){
-    for(var j = copyArr.indexOf(ele); j >= 0; j--){
-      if(ele > copyArr[j] && ele < copyArr[j + 1]){
-        temp = copyArr.splice(idx, 1);
-        copyArr.splice(j + 1, 0, temp[0])
-        // console.log(copyArr);
-        // console.log(copyArr);
-      }
-      if(ele < copyArr[j] && copyArr[j - 1] === undefined){
-        temp = copyArr.splice(idx, 1);
-        copyArr.splice(0, 0, temp[0])
+  const copyArr = array.slice();
+  let temp;
+
+  copyArr.forEach((ele, idx) => {
+    for (let j = idx; j >= 0; j--) {
+      if (j > 0 && ele < copyArr[j - 1]) {
+        temp = copyArr.splice(j, 1);
+        copyArr.splice(j - 1, 0, temp[0]);
       }
     }
-  })
+  });
 
   return copyArr;
 }
-
+// console.log(insertionSort(test))
 module.exports = insertionSort;
