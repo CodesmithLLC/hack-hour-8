e960e9e936b48043a7d6297830a69ed6e1367579
Blake Watkins <iamsoblake@gmail.com>
2016-10-17 09:07:27 -0700

    fixed rotate grid and tested but theres two fixes for tests

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -13,84 +13,34 @@
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
  *
- * BONUS: Do this in place
+ * BONUS: Do this in place 
+ *
  */
 
-// needs some tlc - almost
+// not working for 2x2 or 4x4 grid
 function rotateGrid(grid, n) {
+  const moved = [];
+  let temp;
   for (let i = 0; i < n; i++) {
-    for (let j = n - 1; j >= 0; j--) {
-      // swap
-      let temp = grid[j][n - (i + 1)];
-      grid[j][n - (i + 1)] = grid[i][j];
-      grid[i][j] = temp;
+    for (let j = 0; j < n; j++) {
+      // swap if not 'swapped' yet
+      if (moved.indexOf(grid[j][i]) < 0) {
+        temp = grid[j][i];
+        moved.push(grid[j][i]);
+        grid[j][i] = grid[i][j];
+        grid[i][j] = temp;
+      }
     }
   }
-  return grid;
+  return grid.map(ele => ele.reverse());
+  // return grid;
 }
 
+module.exports = rotateGrid;
+// var sampleGrid = [
+// [1, 2, 3],
+// [4, 5, 6],
+// [7, 8, 9]
+// ];
 
-// function rotateGrid(grid, n) {
-// var temp,
-// lastTemp,
-// i,
-// j,
-// k;
-
-// this loop indicates how many `rotations` take place
-// for (i = 1; i < n; i++) {
-
-// this loop iterates through the 1st dimension
-// for (j = 0; j < grid.length; j++) {
-
-// rotates `top` part of grid
-// if (j === 0) {
-// for (k = grid[j].length - 1; k > 0; k--) {
-// if (k === grid[j].length - 1) {
-// temp = grid[j + 1][grid[j + 1].length - 1];
-// grid[j + 1][grid[j + 1].length - 1] = grid[j][k];
-// grid[j][k] = grid[j][k - 1];
-// } else {
-// grid[j][k] = grid[j][k - 1];
-// }
-// }
-// }
-
-// rotates `bottom` part of grid
-// else if (j === grid.length - 1) {
-// for (k = 0; k < grid[j].length - 1; k++) {
-// if (k === 0) {
-// grid[j - 1][0] = grid[j][k];
-// grid[j][k] = grid[j][k + 1];
-// } else if (k === grid[j].length - 2) {
-// grid[j][k] = temp;
-// } else {
-// grid[j][k] = grid[j][k + 1];
-// }
-// }
-// }
-
-// rotates `middle` sections of grid
-// else {
-// rotates first column
-// grid[j - 1][0] = grid[j][0]
-
-// rotates last column
-// lastTemp = grid[j + 1][grid[j + 1].length - 1];
-// grid[j + 1][grid[j + 1].length - 1] = temp;
-// temp = lastTemp;
-// }
-// }
-// }
-// return grid;
-// }
-
-
-// module.exports = rotateGrid;
-var sampleGrid = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
-
-console.log(rotateGrid(sampleGrid, 3));
+// console.log(rotateGrid(sampleGrid, 3));
